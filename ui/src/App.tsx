8447d11f24ce1439833a4f9a63cda46ca5c17c3c
Liraz Yehezkel <lirazy@up9.com>
2021-04-27 15:10:39 +0300

    css

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import {HarPage} from "./components/HarPage";
+import './App.sass';
 
 const App = () => {
   return (
-      <div style={{backgroundColor: "#090b14", width: "100%"}}>
-        <div style={{height: 100, display: "flex", alignItems: "center", paddingLeft: 24}}>
-            <div style={{fontSize: 45, letterSpacing: 2}}>MIZU</div>
-            <div style={{marginLeft: 10, color: "rgba(255,255,255,0.5)", paddingTop: 15, fontSize: 16}}>Traffic viewer for Kubernetes</div>
+      <div className="mizuApp">
+        <div className="header">
+            <div className="title">MIZU</div>
+            <div className="description">Traffic viewer for Kubernetes</div>
         </div>
         <HarPage/>
       </div>
