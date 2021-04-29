81c6b66999e83491c73896bdb4dca779acf0b2bd
Roee Gadot <roee.gadot@up9.com>
2021-04-29 08:21:30 +0300

    fix multi runner passive tapper command

--- api/scripts/multi-runner.sh
+++ api/scripts/multi-runner.sh
@@ -2,6 +2,6 @@
 
 # this script runs both executables and exits everything if one fails
 ./apiserver &
-./passivetapper -i eth0 &
+./passivetapper -i any -hardump -hardir /tmp/mizuhars -harentriesperfile 50 &
 wait -n
 pkill -P $$
