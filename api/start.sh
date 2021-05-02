2710c1a57a5833f9a75e76c1572eb80b5dcfc374
Roee Gadot <roee.gadot@up9.com>
2021-05-02 14:43:37 +0300

    no message

--- api/start.sh
+++ api/start.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
-./mizuagent -i any -hardump -hardir /tmp/mizuhars -harentriesperfile 1 -targets ${TAPPED_ADDRESSES}
-## this script runs both executables and exits everything if one fails
-#./apiserver -hardir /tmp/mizuhars &
-#./passivetapper -i any -hardump -hardir /tmp/mizuhars -harentriesperfile 5 -targets "${TAPPED_ADDRESSES}" &
-#wait -n
-# pkill -P $$
+./mizuagent -i any -hardump -hardir /tmp/mizuhars -harentriesperfile 5 -targets ${TAPPED_ADDRESSES}
