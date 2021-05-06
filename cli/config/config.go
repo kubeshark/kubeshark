837c0b98423044f0796efe1d272b961ef15573ec
nimrod-up9 <nimrod@up9.com>
2021-05-06 17:23:11 +0300

    gofmt.

--- cli/config/config.go
+++ cli/config/config.go
@@ -1,15 +1,15 @@
 package config
 
 type Options struct {
-	Quiet            bool
-	NoDashboard      bool
-	DashboardPort    uint16
-	Namespace        string
-	AllNamespaces    bool
-	KubeConfigPath   string
-	MizuImage        string
-	MizuPodPort      uint16
-	TappedPodName	string
+	Quiet          bool
+	NoDashboard    bool
+	DashboardPort  uint16
+	Namespace      string
+	AllNamespaces  bool
+	KubeConfigPath string
+	MizuImage      string
+	MizuPodPort    uint16
+	TappedPodName  string
 }
 
 var Configuration = &Options{}
