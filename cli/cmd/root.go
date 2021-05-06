3230e1fd52db08ecf3a325f9708e5ae1fbe457fc
nimrod-up9 <nimrod@up9.com>
2021-05-06 16:17:29 +0300

    tap subcommand works.

---
+++ cli/cmd/root.go
@@ -0,0 +1,18 @@
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "mizu",
+	Short: "A web traffic viewer for kubernetes",
+	Long: `A web traffic viewer for kubernetes
+Further info is available at https://github.com/up9inc/mizu`,
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the tapCmd.
+func Execute() {
+	cobra.CheckErr(rootCmd.Execute())
+}
--- cli/cmd/tap.go
+++ cli/cmd/tap.go
@@ -1,45 +1,42 @@
 package cmd
 
 import (
+	"errors"
+
 	"github.com/spf13/cobra"
+
 	"github.com/up9inc/mizu/cli/config"
 	"github.com/up9inc/mizu/cli/mizu"
 )
 
-// rootCmd represents the base command when called without any subcommands
-var (
-	rootCmd = &cobra.Command{}
-)
-func init() {
-    rootCmd.Use = "mizu"
-	rootCmd.Short = "Tail HTTP traffic from multiple pods"
-	rootCmd.RunE = func(cmd *cobra.Command, args []string) error {
+var tapCmd = &cobra.Command{
+	Use:   "tap",
+	Short: "Tail HTTP traffic from multiple pods",
+	RunE: func(cmd *cobra.Command, args []string) error {
 		if len(args) != 0 {
-			return rootCmd.Help()
+			return errors.New("Unexpected number of arguments")
 		}
-		
+
 		mizu.Run()
 		return nil
-	}
+	},
+}
 
-	rootCmd.Flags().BoolVarP(&config.Configuration.DisplayVersion, "version", "v", false, "Print the version and exit")
-	rootCmd.Flags().BoolVarP(&config.Configuration.Quiet, "quiet", "q", false, "No stdout output")
-	rootCmd.Flags().BoolVarP(&config.Configuration.NoDashboard, "no-dashboard", "", false, "Dont host a dashboard")
-	rootCmd.Flags().Uint16VarP(&config.Configuration.DashboardPort, "dashboard-port", "p", 8899, "Provide a custom port for the dashboard webserver")
-	rootCmd.Flags().StringVarP(&config.Configuration.Namespace, "namespace", "n", "", "Namespace selector")
-	rootCmd.Flags().BoolVarP(&config.Configuration.AllNamespaces, "all-namespaces", "A", false, "Select all namespaces")
-	rootCmd.Flags().StringVarP(&config.Configuration.KubeConfigPath, "kubeconfig", "k", "", "Path to kubeconfig file")
-	rootCmd.Flags().StringVarP(&config.Configuration.MizuImage, "mizu-image", "", "gcr.io/up9-docker-hub/mizu/develop:latest", "Custom image for mizu collector")
-	rootCmd.Flags().Uint16VarP(&config.Configuration.MizuPodPort, "mizu-port", "", 8899, "Port which mizu cli will attempt to forward from the mizu collector pod")
-	rootCmd.Flags().StringVarP(&config.Configuration.TappedPodName, "pod", "", "", "View traffic of this pod")
-	err := rootCmd.MarkFlagRequired("pod")
+func init() {
+	rootCmd.AddCommand(tapCmd)
+
+	tapCmd.Flags().BoolVarP(&config.Configuration.DisplayVersion, "version", "v", false, "Print the version and exit")
+	tapCmd.Flags().BoolVarP(&config.Configuration.Quiet, "quiet", "q", false, "No stdout output")
+	tapCmd.Flags().BoolVarP(&config.Configuration.NoDashboard, "no-dashboard", "", false, "Dont host a dashboard")
+	tapCmd.Flags().Uint16VarP(&config.Configuration.DashboardPort, "dashboard-port", "p", 8899, "Provide a custom port for the dashboard webserver")
+	tapCmd.Flags().StringVarP(&config.Configuration.Namespace, "namespace", "n", "", "Namespace selector")
+	tapCmd.Flags().BoolVarP(&config.Configuration.AllNamespaces, "all-namespaces", "A", false, "Select all namespaces")
+	tapCmd.Flags().StringVarP(&config.Configuration.KubeConfigPath, "kubeconfig", "k", "", "Path to kubeconfig file")
+	tapCmd.Flags().StringVarP(&config.Configuration.MizuImage, "mizu-image", "", "gcr.io/up9-docker-hub/mizu/develop:latest", "Custom image for mizu collector")
+	tapCmd.Flags().Uint16VarP(&config.Configuration.MizuPodPort, "mizu-port", "", 8899, "Port which mizu cli will attempt to forward from the mizu collector pod")
+	tapCmd.Flags().StringVarP(&config.Configuration.TappedPodName, "pod", "", "", "View traffic of this pod")
+	err := tapCmd.MarkFlagRequired("pod")
 	if err != nil {
 		panic(err)
 	}
-}
-
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
-func Execute() {
-	cobra.CheckErr(rootCmd.Execute())
 }
