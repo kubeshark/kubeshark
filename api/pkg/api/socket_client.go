883ebfbc12beca85ae9f9fbc5aa6ec0d9ac6c942
nimrod-up9 <nimrod@up9.com>
2021-05-19 14:12:24 +0300

    Solve connecting issue - Retry a few times.

--- api/pkg/api/socket_client.go
+++ api/pkg/api/socket_client.go
@@ -5,13 +5,33 @@ import (
 	"fmt"
 	"github.com/gorilla/websocket"
 	"mizuserver/pkg/tap"
+	"time"
 )
 
 func ConnectToSocketServer(address string) (*websocket.Conn, error) {
-	connection, _, err := websocket.DefaultDialer.Dial(address, nil)
+	const maxTry = 3
+	const sleepTime = time.Second * 10
+	var err error
+	var connection *websocket.Conn
+	try := 0
+
+	// Connection to server fails if client pod is up before server.
+	// Retries solve this issue.
+	for try < maxTry {
+		connection, _, err = websocket.DefaultDialer.Dial(address, nil)
+		if err != nil {
+			try++
+			fmt.Printf("Failed connecting to websocket server: %s, (%v,%+v)\n", err, err, err)
+		} else {
+			break
+		}
+		time.Sleep(sleepTime)
+	}
+
 	if err != nil {
 		return nil, err
 	}
+
 	return connection, nil
 }
 
