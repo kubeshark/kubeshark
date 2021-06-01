1c4588a83cf8a124b0e3c792a8402817f638b5b4
RamiBerm <rami.berman@up9.com>
2021-06-01 14:41:42 +0300

    Update serializableRegexp.go

--- shared/serializableRegexp.go
+++ shared/serializableRegexp.go
@@ -6,8 +6,6 @@ type SerializableRegexp struct {
 	regexp.Regexp
 }
 
-// CompileRegexToSerializableRegexp wraps the result of the standard library's
-// regexp.Compile, for easy (un)marshaling.
 func CompileRegexToSerializableRegexp(expr string) (*SerializableRegexp, error) {
 	re, err := regexp.Compile(expr)
 	if err != nil {
@@ -16,8 +14,7 @@ func CompileRegexToSerializableRegexp(expr string) (*SerializableRegexp, error) 
 	return &SerializableRegexp{*re}, nil
 }
 
-// UnmarshalText satisfies the encoding.TextMarshaler interface,
-// also used by json.Unmarshal.
+// UnmarshalText is by json.Unmarshal.
 func (r *SerializableRegexp) UnmarshalText(text []byte) error {
 	rr, err := CompileRegexToSerializableRegexp(string(text))
 	if err != nil {
@@ -27,8 +24,7 @@ func (r *SerializableRegexp) UnmarshalText(text []byte) error {
 	return nil
 }
 
-// MarshalText satisfies the encoding.TextMarshaler interface,
-// also used by json.Marshal.
+// MarshalText is used by json.Marshal.
 func (r *SerializableRegexp) MarshalText() ([]byte, error) {
 	return []byte(r.String()), nil
 }
