d93913733e1d747fa8d44bfb12ced86106f5b3d5
nimrod-up9 <nimrod@up9.com>
2021-07-15 13:20:42 +0300

    Moved version parser to shared.

--- shared/semver/semver.go
+++ shared/semver/semver.go
@@ -1,28 +1,28 @@
-package mizu
+package semver
 
 import (
 	"regexp"
 )
 
-type Version string
+type SemVersion string
 
-func (v Version) Breakdown() (string, string, string) {
+func (v SemVersion) Breakdown() (string, string, string) {
 	re := regexp.MustCompile(`\d+`)
 	breakdown := re.FindAllString(string(v), 3)
 	return breakdown[0], breakdown[1], breakdown[2]
 }
 
-func (v Version) Major() string {
+func (v SemVersion) Major() string {
 	major, _, _ := v.Breakdown()
 	return major
 }
 
-func (v Version) Minor() string {
+func (v SemVersion) Minor() string {
 	_, minor, _ := v.Breakdown()
 	return minor
 }
 
-func (v Version) Patch() string {
+func (v SemVersion) Patch() string {
 	_, _, patch := v.Breakdown()
 	return patch
 }
