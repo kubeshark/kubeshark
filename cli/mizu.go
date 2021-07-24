40b8f3b2322f141637a9158303e0171a4fb669d9
Igor Gov <igor.govorov1@gmail.com>
2021-07-24 14:32:49 +0300

    Introducing new logger to file debug and info to stderr

--- cli/mizu.go
+++ cli/mizu.go
@@ -1,7 +1,11 @@
 package main
 
-import "github.com/up9inc/mizu/cli/cmd"
+import (
+	"github.com/up9inc/mizu/cli/cmd"
+	"github.com/up9inc/mizu/cli/mizu"
+)
 
 func main() {
+	mizu.InitLogger()
 	cmd.Execute()
 }
