20708e6423c4d5fd081e9e813a562b987190ddc4
Nimrod Gilboa Markevich <nimrod@up9.com>
2021-08-03 11:39:23 +0300

    Use docker tag instead of building twice.

--- build-push-featurebranch.sh
+++ build-push-featurebranch.sh
@@ -15,14 +15,18 @@ then
   exit 1
 fi
 
-for DOCKER_TAG in ${DOCKER_TAGGED_BUILDS[@]}
+DOCKER_TAG_FIRST="${DOCKER_TAGGED_BUILDS[0]}"
+echo "building $DOCKER_TAG_FIRST"
+docker build -t "$DOCKER_TAG_FIRST" --build-arg SEM_VER=${SEM_VER} --build-arg BUILD_TIMESTAMP=${BUILD_TIMESTAMP} --build-arg GIT_BRANCH=${GIT_BRANCH} --build-arg COMMIT_HASH=${COMMIT_HASH} .
+
+for DOCKER_TAG in "${DOCKER_TAGGED_BUILDS[@]:1}"
 do
-        echo "building $DOCKER_TAG"
-        docker build -t "$DOCKER_TAG" --build-arg SEM_VER=${SEM_VER} --build-arg BUILD_TIMESTAMP=${BUILD_TIMESTAMP} --build-arg GIT_BRANCH=${GIT_BRANCH} --build-arg COMMIT_HASH=${COMMIT_HASH} .
+        echo "tagging as $DOCKER_TAG"
+        docker tag "$DOCKER_TAG_FIRST" "$DOCKER_TAG"
 done
 
-for DOCKER_TAG in ${DOCKER_TAGGED_BUILDS[@]}
+for DOCKER_TAG in "${DOCKER_TAGGED_BUILDS[@]}"
 do
-        echo pushing to "$REPOSITORY"
+        echo pushing "$DOCKER_TAG"
         docker push "$DOCKER_TAG"
 done
