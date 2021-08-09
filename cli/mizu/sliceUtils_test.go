427e86df877dca06239864b7172830ac1defe862
Roy Island <roy@up9.com>
2021-08-09 15:58:00 +0300

    added main branch to test workflow, separated contains unit test cases

--- cli/mizu/sliceUtils_test.go
+++ cli/mizu/sliceUtils_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-func TestContains(t *testing.T) {
+func TestContainsExists(t *testing.T) {
 	tests := []struct {
 		slice         []string
 		containsValue string
@@ -15,10 +15,62 @@ func TestContains(t *testing.T) {
 		{slice: []string{"apple", "orange", "banana", "grapes"}, containsValue: "orange", expected: true},
 		{slice: []string{"apple", "orange", "banana", "grapes"}, containsValue: "banana", expected: true},
 		{slice: []string{"apple", "orange", "banana", "grapes"}, containsValue: "grapes", expected: true},
+	}
+
+	for _, test := range tests {
+		actual := mizu.Contains(test.slice, test.containsValue)
+		if actual != test.expected {
+			t.Errorf("unexpected result - expected: %v, actual: %v", test.expected, actual)
+		}
+	}
+}
+
+func TestContainsNotExists(t *testing.T) {
+	tests := []struct {
+		slice         []string
+		containsValue string
+		expected      bool
+	}{
 		{slice: []string{"apple", "orange", "banana", "grapes"}, containsValue: "cat", expected: false},
 		{slice: []string{"apple", "orange", "banana", "grapes"}, containsValue: "dog", expected: false},
 		{slice: []string{"apple", "orange", "banana", "grapes"}, containsValue: "apples", expected: false},
 		{slice: []string{"apple", "orange", "banana", "grapes"}, containsValue: "rapes", expected: false},
+	}
+
+	for _, test := range tests {
+		actual := mizu.Contains(test.slice, test.containsValue)
+		if actual != test.expected {
+			t.Errorf("unexpected result - expected: %v, actual: %v", test.expected, actual)
+		}
+	}
+}
+
+func TestContainsEmptySlice(t *testing.T) {
+	tests := []struct {
+		slice         []string
+		containsValue string
+		expected      bool
+	}{
+		{slice: []string{}, containsValue: "cat", expected: false},
+		{slice: []string{}, containsValue: "dog", expected: false},
+	}
+
+	for _, test := range tests {
+		actual := mizu.Contains(test.slice, test.containsValue)
+		if actual != test.expected {
+			t.Errorf("unexpected result - expected: %v, actual: %v", test.expected, actual)
+		}
+	}
+}
+
+func TestContainsNilSlice(t *testing.T) {
+	tests := []struct {
+		slice         []string
+		containsValue string
+		expected      bool
+	}{
+		{slice: nil, containsValue: "cat", expected: false},
+		{slice: nil, containsValue: "dog", expected: false},
 	}
 
 	for _, test := range tests {
