e1167c406590a23fd84528febf0a3cc1803791e1
M. Mert Yildiran <mehmet@up9.com>
2021-08-17 05:34:59 +0300

    Remove `github.com/up9inc/mizu/tap/extensions/http/lib`

--- tap/extensions/http/go.mod
+++ tap/extensions/http/go.mod
@@ -1,7 +1,3 @@
 module github.com/up9inc/mizu/tap/extensions/http
 
 go 1.16
-
-require github.com/up9inc/mizu/tap/extensions/http/lib v0.0.0-00010101000000-000000000000
-
-replace github.com/up9inc/mizu/tap/extensions/http/lib => ./lib
--- tap/extensions/http/main.go
+++ tap/extensions/http/main.go
@@ -2,15 +2,12 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/up9inc/mizu/tap/extensions/http/lib"
 )
 
 type greeting string
 
 func (g greeting) Greet() {
 	fmt.Println("Hello HTTP")
-	lib.SomeExample()
 }
 
 // exported as symbol named "Greeter"
