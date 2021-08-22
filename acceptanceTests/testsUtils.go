afd5757315a65dc3ed9054889432555cade3a4b5
RoyUP9 <87927115+RoyUP9@users.noreply.github.com>
2021-08-22 11:38:19 +0300

    added tapper count route and wait time for tappers in test (#226)

--- acceptanceTests/testsUtils.go
+++ acceptanceTests/testsUtils.go
@@ -10,9 +10,15 @@ import (
 	"os/exec"
 	"path"
 	"syscall"
+	"time"
 )
 
-func GetCliPath() (string, error) {
+const (
+	LongRetriesCount = 100
+	ShortRetriesCount = 10
+)
+
+func getCliPath() (string, error) {
 	dir, filePathErr := os.Getwd()
 	if filePathErr != nil {
 		return "", filePathErr
@@ -22,34 +28,74 @@ func GetCliPath() (string, error) {
 	return cliPath, nil
 }
 
-func GetDefaultCommandArgs() []string {
+func getDefaultCommandArgs() []string {
 	setFlag := "--set"
 	telemetry := "telemetry=false"
 
 	return []string{setFlag, telemetry}
 }
 
-func GetDefaultTapCommandArgs() []string {
+func getDefaultTapCommandArgs() []string {
 	tapCommand := "tap"
 	setFlag := "--set"
 	namespaces := "tap.namespaces=mizu-tests"
 	agentImage := "agent-image=gcr.io/up9-docker-hub/mizu/ci:0.0.0"
 	imagePullPolicy := "image-pull-policy=Never"
 
-	defaultCmdArgs := GetDefaultCommandArgs()
+	defaultCmdArgs := getDefaultCommandArgs()
 
 	return append([]string{tapCommand, setFlag, namespaces, setFlag, agentImage, setFlag, imagePullPolicy}, defaultCmdArgs...)
 }
 
-func GetDefaultFetchCommandArgs() []string {
+func getDefaultFetchCommandArgs() []string {
 	tapCommand := "fetch"
 
-	defaultCmdArgs := GetDefaultCommandArgs()
+	defaultCmdArgs := getDefaultCommandArgs()
 
 	return append([]string{tapCommand}, defaultCmdArgs...)
 }
 
-func JsonBytesToInterface(jsonBytes []byte) (interface{}, error) {
+func retriesExecute(retriesCount int, executeFunc func() error) error {
+	var lastError error
+
+	for i := 0; i < retriesCount; i++ {
+		if err := executeFunc(); err != nil {
+			lastError = err
+
+			time.Sleep(1 * time.Second)
+			continue
+		}
+
+		return nil
+	}
+
+	return fmt.Errorf("reached max retries count, retries count: %v, last err: %v", retriesCount, lastError)
+}
+
+func waitTapPodsReady() error {
+	resolvingUrl := fmt.Sprintf("http://localhost:8899/mizu/status/tappersCount")
+	tapPodsReadyFunc := func() error {
+		requestResult, requestErr := executeHttpRequest(resolvingUrl)
+		if requestErr != nil {
+			return requestErr
+		}
+
+		tappersCount, ok := requestResult.(float64)
+		if !ok {
+			return fmt.Errorf("invalid tappers count type")
+		}
+
+		if tappersCount == 0 {
+			return fmt.Errorf("no tappers running")
+		}
+
+		return nil
+	}
+
+	return retriesExecute(LongRetriesCount, tapPodsReadyFunc)
+}
+
+func jsonBytesToInterface(jsonBytes []byte) (interface{}, error) {
 	var result interface{}
 	if parseErr := json.Unmarshal(jsonBytes, &result); parseErr != nil {
 		return nil, parseErr
@@ -58,7 +104,7 @@ func JsonBytesToInterface(jsonBytes []byte) (interface{}, error) {
 	return result, nil
 }
 
-func ExecuteHttpRequest(url string) (interface{}, error) {
+func executeHttpRequest(url string) (interface{}, error) {
 	response, requestErr := http.Get(url)
 	if requestErr != nil {
 		return nil, requestErr
@@ -66,15 +112,17 @@ func ExecuteHttpRequest(url string) (interface{}, error) {
 		return nil, fmt.Errorf("invalid status code %v", response.StatusCode)
 	}
 
+	defer func() { response.Body.Close() }()
+
 	data, readErr := ioutil.ReadAll(response.Body)
 	if readErr != nil {
 		return nil, readErr
 	}
 
-	return JsonBytesToInterface(data)
+	return jsonBytesToInterface(data)
 }
 
-func CleanupCommand(cmd *exec.Cmd) error {
+func cleanupCommand(cmd *exec.Cmd) error {
 	if err := cmd.Process.Signal(syscall.SIGQUIT); err != nil {
 		return err
 	}
@@ -86,8 +134,8 @@ func CleanupCommand(cmd *exec.Cmd) error {
 	return nil
 }
 
-func GetEntriesFromHarBytes(harBytes []byte) ([]interface{}, error){
-	harInterface, convertErr := JsonBytesToInterface(harBytes)
+func getEntriesFromHarBytes(harBytes []byte) ([]interface{}, error){
+	harInterface, convertErr := jsonBytesToInterface(harBytes)
 	if convertErr != nil {
 		return nil, convertErr
 	}
@@ -97,14 +145,12 @@ func GetEntriesFromHarBytes(harBytes []byte) ([]interface{}, error){
 		return nil, errors.New("invalid har type")
 	}
 
-	harLogInterface := har["log"]
-	harLog, ok :=  harLogInterface.(map[string]interface{})
+	harLog, ok :=  har["log"].(map[string]interface{})
 	if !ok {
 		return nil, errors.New("invalid har log type")
 	}
 
-	harEntriesInterface := harLog["entries"]
-	harEntries, ok := harEntriesInterface.([]interface{})
+	harEntries, ok := harLog["entries"].([]interface{})
 	if !ok {
 		return nil, errors.New("invalid har entries type")
 	}
--- agent/pkg/api/socket_server_handlers.go
+++ agent/pkg/api/socket_server_handlers.go
@@ -28,6 +28,7 @@ func init() {
 func (h *RoutesEventHandlers) WebSocketConnect(socketId int, isTapper bool) {
 	if isTapper {
 		rlog.Infof("Websocket event - Tapper connected, socket ID: %d", socketId)
+		providers.TapperAdded()
 	} else {
 		rlog.Infof("Websocket event - Browser socket connected, socket ID: %d", socketId)
 		socketListLock.Lock()
@@ -39,6 +40,7 @@ func (h *RoutesEventHandlers) WebSocketConnect(socketId int, isTapper bool) {
 func (h *RoutesEventHandlers) WebSocketDisconnect(socketId int, isTapper bool) {
 	if isTapper {
 		rlog.Infof("Websocket event - Tapper disconnected, socket ID:  %d", socketId)
+		providers.TapperRemoved()
 	} else {
 		rlog.Infof("Websocket event - Browser socket disconnected, socket ID:  %d", socketId)
 		socketListLock.Lock()
