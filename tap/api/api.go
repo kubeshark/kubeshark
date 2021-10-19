6d11796ac5cea67037ffdc27334245fbb5c3d55d
M. Mert Yildiran <mehmet@up9.com>
2021-10-19 16:09:29 +0300

    Remove two more lines related to logging

--- tap/api/api.go
+++ tap/api/api.go
@@ -7,7 +7,6 @@ import (
 	"errors"
 	"fmt"
 	"io/ioutil"
-	"log"
 	"net/http"
 	"plugin"
 	"sync"
@@ -251,7 +250,6 @@ func (h HTTPPayload) MarshalJSON() ([]byte, error) {
 	case TypeHttpRequest:
 		harRequest, err := har.NewRequest(h.Data.(*http.Request), true)
 		if err != nil {
-			log.Printf("Failed converting request to HAR %s (%v,%+v)", err, err, err)
 			return nil, errors.New("Failed converting request to HAR")
 		}
 		return json.Marshal(&HTTPWrapper{
@@ -263,7 +261,6 @@ func (h HTTPPayload) MarshalJSON() ([]byte, error) {
 	case TypeHttpResponse:
 		harResponse, err := har.NewResponse(h.Data.(*http.Response), true)
 		if err != nil {
-			log.Printf("Failed converting response to HAR %s (%v,%+v)", err, err, err)
 			return nil, errors.New("Failed converting response to HAR")
 		}
 		return json.Marshal(&HTTPWrapper{
