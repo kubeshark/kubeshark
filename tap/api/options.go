005d740e6c5aeace52459257deea0db14db70485
Rami Berman <rami.berman@up9.com>
2021-10-28 10:32:22 +0300

    Update options.go and serializable_regexp.go

--- tap/api/options.go
+++ tap/api/options.go
@@ -1,9 +1,7 @@
 package api
 
-import "github.com/up9inc/mizu/shared"
-
 type TrafficFilteringOptions struct {
 	IgnoredUserAgents       []string
-	PlainTextMaskingRegexes []*shared.SerializableRegexp
+	PlainTextMaskingRegexes []*SerializableRegexp
 	DisableRedaction        bool
 }
