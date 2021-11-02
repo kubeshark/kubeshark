fd7e887096561cd2da26ab3a4578cee0da70881b
David Levanon <dvdlevanon@gmail.com>
2021-11-02 16:55:01 +0200

    map /proc of host to tapper

--- cli/kubernetes/provider.go
+++ cli/kubernetes/provider.go
@@ -54,6 +54,8 @@ type Provider struct {
 
 const (
 	fieldManagerName = "mizu-manager"
+	procfsVolumeName = "proc"
+	procfsMountPath  = "/hostproc"
 )
 
 func NewProvider(kubeConfigPath string) (*Provider, error) {
@@ -539,6 +541,7 @@ func (provider *Provider) ApplyMizuTapperDaemonSet(ctx context.Context, namespac
 		"--tap",
 		"--api-server-address", fmt.Sprintf("ws://%s/wsTapper", apiServerPodIp),
 		"--nodefrag",
+		"--procfs", procfsMountPath,
 	}
 
 	debugMode := ""
@@ -617,6 +620,10 @@ func (provider *Provider) ApplyMizuTapperDaemonSet(ctx context.Context, namespac
 	noScheduleToleration.WithOperator(core.TolerationOpExists)
 	noScheduleToleration.WithEffect(core.TaintEffectNoSchedule)
 
+	procfsVolume := applyconfcore.Volume()
+	procfsVolume.WithName(procfsVolumeName).WithHostPath(applyconfcore.HostPathVolumeSource().WithPath("/proc"))
+	agentContainer.WithVolumeMounts(applyconfcore.VolumeMount().WithName(procfsVolumeName).WithMountPath(procfsMountPath))
+
 	podSpec := applyconfcore.PodSpec()
 	podSpec.WithHostNetwork(true)
 	podSpec.WithDNSPolicy(core.DNSClusterFirstWithHostNet)
@@ -627,6 +634,7 @@ func (provider *Provider) ApplyMizuTapperDaemonSet(ctx context.Context, namespac
 	podSpec.WithContainers(agentContainer)
 	podSpec.WithAffinity(affinity)
 	podSpec.WithTolerations(noExecuteToleration, noScheduleToleration)
+	podSpec.WithVolumes(procfsVolume)
 
 	podTemplate := applyconfcore.PodTemplateSpec()
 	podTemplate.WithLabels(map[string]string{"app": tapperPodName})
