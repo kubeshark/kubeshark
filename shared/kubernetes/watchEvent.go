cc7691155bae2ca082d4c5690f8b0b475323a57c
Nimrod Gilboa Markevich <nimrod@up9.com>
2021-11-21 16:59:55 +0200

    Add information to error message

--- shared/kubernetes/watchEvent.go
+++ shared/kubernetes/watchEvent.go
@@ -2,18 +2,28 @@ package kubernetes
 
 import (
 	"fmt"
+	"reflect"
 
 	corev1 "k8s.io/api/core/v1"
 	eventsv1 "k8s.io/api/events/v1"
 	"k8s.io/apimachinery/pkg/watch"
 )
 
+type InvalidObjectType struct {
+	RequestedType reflect.Type
+}
+
+// Implements the error interface
+func (iot *InvalidObjectType) Error() string {
+	return fmt.Sprintf("Cannot convert event to type %s", iot.RequestedType)
+}
+
 type WatchEvent watch.Event
 
 func (we *WatchEvent) ToPod() (*corev1.Pod, error) {
 	pod, ok := we.Object.(*corev1.Pod)
 	if !ok {
-		return nil, fmt.Errorf("Invalid object type on pod event stream")
+		return nil, &InvalidObjectType{RequestedType: reflect.TypeOf(pod)}
 	}
 
 	return pod, nil
@@ -22,7 +32,7 @@ func (we *WatchEvent) ToPod() (*corev1.Pod, error) {
 func (we *WatchEvent) ToEvent() (*eventsv1.Event, error) {
 	event, ok := we.Object.(*eventsv1.Event)
 	if !ok {
-		return nil, fmt.Errorf("Invalid object type on pod event stream")
+		return nil, &InvalidObjectType{RequestedType: reflect.TypeOf(event)}
 	}
 
 	return event, nil
