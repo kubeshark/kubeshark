13bd645d8006a8ff0749b7f1cdca544a47584e19
M. Mert Yildiran <mehmet@up9.com>
2021-11-25 05:11:03 +0300

    Rename `URL` field to `Target URI` in the UI to prevent confusion

--- tap/extensions/http/main.go
+++ tap/extensions/http/main.go
@@ -162,6 +162,7 @@ func (d dissecting) Analyze(item *api.OutputChannelItem, resolvedSource string, 
 	}
 
 	request["url"] = reqDetails["url"].(string)
+	reqDetails["targetUri"] = reqDetails["url"]
 	reqDetails["path"] = path
 	reqDetails["summary"] = path
 
@@ -269,9 +270,9 @@ func representRequest(request map[string]interface{}) (repRequest []interface{})
 			Selector: `request.method`,
 		},
 		{
-			Name:     "URL",
-			Value:    request["url"].(string),
-			Selector: `request.url`,
+			Name:     "Target URI",
+			Value:    request["targetUri"].(string),
+			Selector: `request.targetUri`,
 		},
 		{
 			Name:     "Path",
