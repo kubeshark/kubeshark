2bbcba4b4017e190402768ce144bf7a8855afb16
M. Mert Yildiran <mehmet@up9.com>
2021-12-01 10:33:12 +0300

    Update the route comment

--- agent/pkg/routes/entries_routes.go
+++ agent/pkg/routes/entries_routes.go
@@ -10,6 +10,6 @@ import (
 func EntriesRoutes(ginApp *gin.Engine) {
 	routeGroup := ginApp.Group("/entries")
 
-	routeGroup.GET("/", controllers.GetEntries)  // get entries (base/thin entries)
+	routeGroup.GET("/", controllers.GetEntries)  // get entries (base/thin entries) and metadata
 	routeGroup.GET("/:id", controllers.GetEntry) // get single (full) entry
 }
