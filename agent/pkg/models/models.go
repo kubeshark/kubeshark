3d88ae84f41a1c8060bac0395f39de6f9c6f7760
M. Mert Yildiran <mehmet@up9.com>
2021-12-01 10:23:08 +0300

    Accept negative `leftOff` value

--- agent/pkg/models/models.go
+++ agent/pkg/models/models.go
@@ -17,7 +17,7 @@ func GetEntry(r *tapApi.MizuEntry, v tapApi.DataUnmarshaler) error {
 }
 
 type EntriesRequest struct {
-	LeftOff   int    `form:"leftOff" validate:"required,min=0,max=2147483647"`
+	LeftOff   int    `form:"leftOff" validate:"required,min=-1,max=2147483647"`
 	Direction int    `form:"direction" validate:"required,oneof='1' '-1'"`
 	Query     string `form:"query"`
 	Limit     int    `form:"limit" validate:"required,min=1,max=2147483647"`
