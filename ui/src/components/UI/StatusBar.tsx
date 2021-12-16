497c8b297cd32acff8f27cebd768ff9aa5252b7a
Liraz Yehezkel <lirazy@up9.com>
2021-12-16 17:52:28 +0200

    is tapping pod visualization

--- ui/src/components/UI/StatusBar.tsx
+++ ui/src/components/UI/StatusBar.tsx
@@ -1,9 +1,13 @@
 import './style/StatusBar.sass';
 import React, {useState} from "react";
+import warningIcon from '../assets/warning_icon.svg';
+import failIcon from '../assets/failed.svg';
+import successIcon from '../assets/success.svg';
 
 export interface TappingStatusPod {
     name: string;
     namespace: string;
+    isTapped: boolean;
 }
 
 export interface TappingStatus {
@@ -11,7 +15,7 @@ export interface TappingStatus {
 }
 
 export interface Props {
-    tappingStatus: TappingStatus
+    tappingStatus: TappingStatusPod[]
 }
 
 const pluralize = (noun: string, amount: number) => {
@@ -22,23 +26,29 @@ export const StatusBar: React.FC<Props> = ({tappingStatus}) => {
 
     const [expandedBar, setExpandedBar] = useState(false);
 
-    const uniqueNamespaces = Array.from(new Set(tappingStatus.pods.map(pod => pod.namespace)));
-    const amountOfPods = tappingStatus.pods.length;
+    const uniqueNamespaces = Array.from(new Set(tappingStatus.map(pod => pod.namespace)));
+    const amountOfPods = tappingStatus.length;
+    const amountOfTappedPods = tappingStatus.filter(pod => pod.isTapped).length;
+    const amountOfUntappedPods = amountOfPods - amountOfTappedPods;
 
     return <div className={'statusBar' + (expandedBar ? ' expandedStatusBar' : "")} onMouseOver={() => setExpandedBar(true)} onMouseLeave={() => setExpandedBar(false)}>
-        <div className="podsCount">{`Tapping ${amountOfPods} ${pluralize('pod', amountOfPods)} in ${pluralize('namespace', uniqueNamespaces.length)} ${uniqueNamespaces.join(", ")}`}</div>
+        <div className="podsCount">
+            {tappingStatus.some(pod => !pod.isTapped) && <img src={warningIcon} alt="warning"/>}
+            {`Tapping ${amountOfUntappedPods > 0 ? amountOfTappedPods + " / " + amountOfPods : amountOfPods} ${pluralize('pod', amountOfPods)} in ${pluralize('namespace', uniqueNamespaces.length)} ${uniqueNamespaces.join(", ")}`}</div>
         {expandedBar && <div style={{marginTop: 20}}>
             <table>
                 <thead>
                     <tr>
                         <th>Pod name</th>
                         <th>Namespace</th>
+                        <th style={{marginLeft: 10}}>Tapping</th>
                     </tr>
                 </thead>
                 <tbody>
-                    {tappingStatus.pods.map(pod => <tr key={pod.name}>
+                    {tappingStatus.map(pod => <tr key={pod.name}>
                         <td>{pod.name}</td>
                         <td>{pod.namespace}</td>
+                        <td style={{textAlign: "center"}}><img style={{height: 20}} alt="status" src={pod.isTapped ? successIcon : failIcon}/></td>
                     </tr>)}
                 </tbody>
             </table>
