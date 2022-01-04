cbada39ab180102ada3b67dd3af16fbae284cd46
Adam Kol <adam@up9.com>
2022-01-04 17:54:00 +0200

    now works

--- acceptanceTests/cypress/integration/tests/GuiPort.js
+++ acceptanceTests/cypress/integration/tests/GuiPort.js
@@ -1,6 +1,5 @@
 it('check', function () {
-    // cy.visit(`http://localhost:${Cypress.env('port')}/`)
-    cy.visit(`http://localhost:8899/`)
+    cy.visit(`http://localhost:${Cypress.env('port')}/`)
 
     cy.get('.header').should('be.visible')
     cy.get('.TrafficPageHeader').should('be.visible')
