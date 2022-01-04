02b2cbaa03a7ce581315fa0fd4def336816d04f5
RoyUP9 <87927115+RoyUP9@users.noreply.github.com>
2022-01-04 17:27:14 +0200

    Added update config route for install mode (#581)

--- agent/pkg/models/models.go
+++ agent/pkg/models/models.go
@@ -16,6 +16,10 @@ func GetEntry(r *tapApi.MizuEntry, v tapApi.DataUnmarshaler) error {
 	return v.UnmarshalData(r)
 }
 
+type StandaloneTapConfig struct {
+	TappedNamespaces map[string]bool `json:"tappedNamespaces"`
+}
+
 type EntriesRequest struct {
 	LeftOff   int    `form:"leftOff" validate:"required,min=-1"`
 	Direction int    `form:"direction" validate:"required,oneof='1' '-1'"`
