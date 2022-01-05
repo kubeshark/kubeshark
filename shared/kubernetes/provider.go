87e20a14b108559a9109390bf785ba25ddf815f1
Rami Berman <rami.berman@up9.com>
2022-01-05 14:59:12 +0200

    Update provider.go

--- shared/kubernetes/provider.go
+++ shared/kubernetes/provider.go
@@ -378,7 +378,7 @@ func (provider *Provider) CreateService(ctx context.Context, namespace string, s
 			},
 		},
 		Spec: core.ServiceSpec{
-			Ports:    []core.ServicePort{{TargetPort: intstr.FromInt(shared.DefaultApiServerPort), Port: 80}},
+			Ports:    []core.ServicePort{{TargetPort: intstr.FromInt(shared.DefaultApiServerPort), Port: 80, Name: "api"}},
 			Type:     core.ServiceTypeNodePort,
 			Selector: map[string]string{"app": appLabelValue},
 		},
