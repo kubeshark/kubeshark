c0f78980f936d34901fcc35e81fb023397e6ba12
Rami Berman <rami.berman@up9.com>
2022-01-05 15:59:51 +0200

    Update start.sh

--- agent/kratos/start.sh
+++ agent/kratos/start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 kratos migrate sql sqlite:///app/data/kratos.sqlite?_fk=true --yes # this initializes the db
-kratos serve -c /etc/config/kratos/kratos.yml --dev --watch-courier # start kratos
+kratos serve -c /etc/config/kratos/kratos.yml --watch-courier # start kratos
