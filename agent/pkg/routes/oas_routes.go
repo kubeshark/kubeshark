cc53d7c31feebe1c9b47fbba690248903df3e21f
Andrey Pokhilko <apc4@ya.ru>
2022-01-05 18:20:46 +0300

    Add all specs getter

--- agent/pkg/routes/oas_routes.go
+++ agent/pkg/routes/oas_routes.go
@@ -10,6 +10,7 @@ import (
 func OASRoutes(ginApp *gin.Engine) {
 	routeGroup := ginApp.Group("/oas")
 
-	routeGroup.GET("/", controllers.GetOASServers) // list of servers in OAS map
-	routeGroup.GET("/:id", controllers.GetOASSpec) // get OAS spec for given server
+	routeGroup.GET("/", controllers.GetOASServers)     // list of servers in OAS map
+	routeGroup.GET("/all", controllers.GetOASAllSpecs) // list of servers in OAS map
+	routeGroup.GET("/:id", controllers.GetOASSpec)     // get OAS spec for given server
 }
