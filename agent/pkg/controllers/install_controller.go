293d7c4a497420c31790a8c118220b90dfb91d78
Rami Berman <rami.berman@up9.com>
2022-01-05 15:43:39 +0200

    Update install_controller.go

--- agent/pkg/controllers/install_controller.go
+++ agent/pkg/controllers/install_controller.go
@@ -5,10 +5,12 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/up9inc/mizu/shared/logger"
 )
 
 func IsSetupNecessary(c *gin.Context) {
 	if IsInstallNeeded, err := providers.IsInstallNeeded(); err != nil {
+		logger.Log.Errorf("unknown internal while checking if install is needed %s", err)
 		c.AbortWithStatusJSON(500, gin.H{"error": "internal error occured while checking if install is needed"})
 	} else {
 		c.JSON(http.StatusOK, IsInstallNeeded)
