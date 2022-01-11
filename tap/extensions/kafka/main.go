80e881fee2661610c836979a6864dd85d182f267
M. Mert Yıldıran <mehmet@up9.com>
2022-01-11 12:51:30 +0300

    Upgrade Basenine to `0.3.0`, do a refactor to enable `redact` helper and update the cheatsheet (#614)
    * Upgrade Basenine version from `0.2.26` to `0.3.0`
    
    * Remove `Summarize` method from `Dissector` interface and refactor the data structures in `tap/api/api.go`
    
    * Rename `MizuEntry` to `Entry` and `BaseEntryDetails` to `BaseEntry`
    
    * Populate `ContractStatus` field as well
    
    * Update the cheatsheet
    
    * Upgrade the Basenine version in the helm chart as well
    
    * Remove a forgoten `console.log` call

--- tap/extensions/kafka/main.go
+++ tap/extensions/kafka/main.go
@@ -62,7 +62,7 @@ func (d dissecting) Dissect(b *bufio.Reader, isClient bool, tcpID *api.TcpID, co
 	}
 }
 
-func (d dissecting) Analyze(item *api.OutputChannelItem, resolvedSource string, resolvedDestination string) *api.MizuEntry {
+func (d dissecting) Analyze(item *api.OutputChannelItem, resolvedSource string, resolvedDestination string) *api.Entry {
 	request := item.Pair.Request.Payload.(map[string]interface{})
 	reqDetails := request["details"].(map[string]interface{})
 	apiKey := ApiKey(reqDetails["apiKey"].(float64))
@@ -146,7 +146,7 @@ func (d dissecting) Analyze(item *api.OutputChannelItem, resolvedSource string, 
 	if elapsedTime < 0 {
 		elapsedTime = 0
 	}
-	return &api.MizuEntry{
+	return &api.Entry{
 		Protocol: _protocol,
 		Source: &api.TCP{
 			Name: resolvedSource,
@@ -168,25 +168,6 @@ func (d dissecting) Analyze(item *api.OutputChannelItem, resolvedSource string, 
 		ElapsedTime: elapsedTime,
 		Summary:     summary,
 		IsOutgoing:  item.ConnectionInfo.IsOutgoing,
-	}
-}
-
-func (d dissecting) Summarize(entry *api.MizuEntry) *api.BaseEntryDetails {
-	return &api.BaseEntryDetails{
-		Id:          entry.Id,
-		Protocol:    _protocol,
-		Summary:     entry.Summary,
-		StatusCode:  entry.Status,
-		Method:      entry.Method,
-		Timestamp:   entry.Timestamp,
-		Source:      entry.Source,
-		Destination: entry.Destination,
-		IsOutgoing:  entry.IsOutgoing,
-		Latency:     entry.ElapsedTime,
-		Rules: api.ApplicableRules{
-			Latency: 0,
-			Status:  false,
-		},
 	}
 }
 
