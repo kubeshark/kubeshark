f88b18d7e79002d29fc3dbbacd2535b96bc19f7e
Andrey Pokhilko <apc4@ya.ru>
2022-01-11 19:45:19 +0300

    add required import

--- agent/pkg/routes/oas_routes.go
+++ agent/pkg/routes/oas_routes.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	"mizuserver/pkg/controllers"
+	"mizuserver/pkg/middlewares"
 
 	"github.com/gin-gonic/gin"
 )
@@ -9,7 +10,8 @@ import (
 // OASRoutes methods to access OAS spec
 func OASRoutes(ginApp *gin.Engine) {
 	routeGroup := ginApp.Group("/oas")
-routeGroup.Use(middlewares.RequiresAuth())
+	routeGroup.Use(middlewares.RequiresAuth())
+
 	routeGroup.GET("/", controllers.GetOASServers)     // list of servers in OAS map
 	routeGroup.GET("/all", controllers.GetOASAllSpecs) // list of servers in OAS map
 	routeGroup.GET("/:id", controllers.GetOASSpec)     // get OAS spec for given server
