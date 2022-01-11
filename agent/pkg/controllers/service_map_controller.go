bc9cf8b9bb997d0ce96e6d9a094a96335b24b43e
Gustavo Massaneiro <gustavomassa123@gmail.com>
2022-01-11 16:15:05 -0300

    new service property

--- agent/pkg/controllers/service_map_controller.go
+++ agent/pkg/controllers/service_map_controller.go
@@ -8,29 +8,30 @@ import (
 	"github.com/up9inc/mizu/shared"
 )
 
-type ServiceMapController struct{}
+type ServiceMapController struct {
+	service api.ServiceMap
+}
 
 func NewServiceMapController() *ServiceMapController {
-	return &ServiceMapController{}
+	return &ServiceMapController{
+		service: api.GetServiceMapInstance(),
+	}
 }
 
 func (s *ServiceMapController) Status(c *gin.Context) {
-	serviceMap := api.GetServiceMapInstance()
-	c.JSON(http.StatusOK, serviceMap.GetStatus())
+	c.JSON(http.StatusOK, s.service.GetStatus())
 }
 
 func (s *ServiceMapController) Get(c *gin.Context) {
-	serviceMap := api.GetServiceMapInstance()
 	response := &shared.ServiceMapResponse{
-		Status: serviceMap.GetStatus(),
-		Nodes:  serviceMap.GetNodes(),
-		Edges:  serviceMap.GetEdges(),
+		Status: s.service.GetStatus(),
+		Nodes:  s.service.GetNodes(),
+		Edges:  s.service.GetEdges(),
 	}
 	c.JSON(http.StatusOK, response)
 }
 
 func (s *ServiceMapController) Reset(c *gin.Context) {
-	serviceMap := api.GetServiceMapInstance()
-	serviceMap.Reset()
+	s.service.Reset()
 	s.Status(c)
 }
