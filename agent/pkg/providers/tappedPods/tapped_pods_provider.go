26a9c31d1e9f80a85f29f7409fdfd9d27630d4a7
RoyUP9 <87927115+RoyUP9@users.noreply.github.com>
2022-01-12 16:03:50 +0200

    Extracted agent status to consistent volume (#628)

--- agent/pkg/providers/tappedPods/tapped_pods_provider.go
+++ agent/pkg/providers/tappedPods/tapped_pods_provider.go
@@ -2,25 +2,49 @@ package tappedPods
 
 import (
 	"github.com/up9inc/mizu/shared"
-	"mizuserver/pkg/providers/tappersStatus"
+	"github.com/up9inc/mizu/shared/logger"
+	"mizuserver/pkg/providers/tappers"
+	"mizuserver/pkg/utils"
+	"os"
 	"strings"
+	"sync"
 )
 
-var tappedPods []*shared.PodInfo
+const FilePath = shared.DataDirPath + "tapped-pods.json"
+
+var (
+	lock       = &sync.Mutex{}
+	syncOnce   sync.Once
+	tappedPods []*shared.PodInfo
+)
 
 func Get() []*shared.PodInfo {
+	syncOnce.Do(func() {
+		if err := utils.ReadJsonFile(FilePath, &tappedPods); err != nil {
+			if !os.IsNotExist(err) {
+				logger.Log.Errorf("Error reading tapped pods from file, err: %v", err)
+			}
+		}
+	})
+
 	return tappedPods
 }
 
 func Set(tappedPodsToSet []*shared.PodInfo) {
+	lock.Lock()
+	defer lock.Unlock()
+
 	tappedPods = tappedPodsToSet
+	if err := utils.SaveJsonFile(FilePath, tappedPods); err != nil {
+		logger.Log.Errorf("Error saving tapped pods, err: %v", err)
+	}
 }
 
 func GetTappedPodsStatus() []shared.TappedPodStatus {
 	tappedPodsStatus := make([]shared.TappedPodStatus, 0)
 	for _, pod := range Get() {
 		var status string
-		if tapperStatus, ok := tappersStatus.Get()[pod.NodeName]; ok {
+		if tapperStatus, ok := tappers.GetStatus()[pod.NodeName]; ok {
 			status = strings.ToLower(tapperStatus.Status)
 		}
 
