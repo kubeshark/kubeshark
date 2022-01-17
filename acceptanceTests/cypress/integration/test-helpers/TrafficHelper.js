f6a50e6621d65e4599025c6adc8d81a7576a41e0
Adam Kol <adam@up9.com>
2022-01-17 10:36:26 +0200

    no message

--- acceptanceTests/cypress/integration/test-helpers/TrafficHelper.js
+++ acceptanceTests/cypress/integration/test-helpers/TrafficHelper.js
@@ -1,7 +1,7 @@
 export function check(shouldInclude, content, domPathToContainer){
     it(`should ${shouldInclude ? '' : 'not'} include '${content}'`, function () {
-        cy.get(domPathToContainer).then(HTMLtext => {
-            const allTextString = HTMLtext.text();
+        cy.get(domPathToContainer).then(htmlText => {
+            const allTextString = htmlText.text();
             if (allTextString.includes(content) !== shouldInclude)
                 throw new Error(`One of the containers part contains ${content}`)
         });
