e8d7cd07514df01d113eb6bbcf3dfbce47fc8786
M. Mert Yildiran <mehmet@up9.com>
2022-01-17 22:44:02 +0300

    Upgrade the Basenine version from `v0.3.0` to `v0.4.6`

--- shared/consts.go
+++ shared/consts.go
@@ -17,5 +17,5 @@ const (
 	BasenineHost                     = "127.0.0.1"
 	BaseninePort                     = "9099"
 	BasenineImageRepo                = "ghcr.io/up9inc/basenine"
-	BasenineImageTag                 = "v0.3.0"
+	BasenineImageTag                 = "v0.4.6"
 )
