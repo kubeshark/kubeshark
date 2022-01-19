08bc2d29bb432bf9928462fe82bbbc8a0015d3da
Roee Gadot <roee.gadot@up9.com>
2022-01-19 13:01:26 +0200

    no message

--- cli/config/configStructs/viewConfig.go
+++ cli/config/configStructs/viewConfig.go
@@ -1,11 +1,11 @@
 package configStructs
 
 const (
-	GuiPortViewName   = "gui-port"
-	UrlViewName       = "url"
+	GuiPortViewName = "gui-port"
+	UrlViewName     = "url"
 )
 
 type ViewConfig struct {
-	GuiPort   uint16 `yaml:"gui-port" default:"8899"`
-	Url       string `yaml:"url,omitempty" readonly:""`
+	GuiPort uint16 `yaml:"gui-port" default:"8899"`
+	Url     string `yaml:"url,omitempty" readonly:""`
 }
