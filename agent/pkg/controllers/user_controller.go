50c89e62451fcb62060f5c43142e3dd8d6244d31
RamiBerm <54766858+RamiBerm@users.noreply.github.com>
2022-01-20 16:29:23 +0200

    Revert "TRA-4157 fix ws auth (#669)" (#671)
    This reverts commit 676e50b0b1346f008a9f22b13636dd5d9ed9fa69.

--- agent/pkg/controllers/user_controller.go
+++ agent/pkg/controllers/user_controller.go
@@ -1,9 +1,7 @@
 package controllers
 
 import (
-	"errors"
 	"mizuserver/pkg/providers"
-	"net/http"
 
 	"github.com/gin-gonic/gin"
 	"github.com/up9inc/mizu/shared/logger"
@@ -13,44 +11,20 @@ func Login(c *gin.Context) {
 	if token, err := providers.PerformLogin(c.PostForm("username"), c.PostForm("password"), c.Request.Context()); err != nil {
 		c.AbortWithStatusJSON(401, gin.H{"error": "bad login"})
 	} else {
-		c.SetSameSite(http.SameSiteLaxMode)
-		c.SetCookie("x-session-token", *token, 3600, "/", "", false, true)
-		c.JSON(200, "")
+		c.JSON(200, gin.H{"token": token})
 	}
 }
 
 func Logout(c *gin.Context) {
-	token, err := c.Cookie("x-session-token")
-	if err != nil {
-		if errors.Is(err, http.ErrNoCookie) {
-			c.AbortWithStatusJSON(401, gin.H{"error": "could not find session cookie"})
-		} else {
-			logger.Log.Errorf("error reading cookie in logout %s", err)
-			c.AbortWithStatusJSON(500, gin.H{"error": "error occured while logging out, the session might still be valid"})
-		}
-
-		return
-	}
-
-	if err = providers.Logout(token, c.Request.Context()); err != nil {
+	token := c.GetHeader("x-session-token")
+	if err := providers.Logout(token, c.Request.Context()); err != nil {
 		c.AbortWithStatusJSON(500, gin.H{"error": "error occured while logging out, the session might still be valid"})
 	} else {
-		c.SetCookie("x-session-token", "", -1, "/", "", false, true)
 		c.JSON(200, "")
 	}
 }
 
 func Register(c *gin.Context) {
-	// only allow one user to be created without authentication
-	if IsInstallNeeded, err := providers.IsInstallNeeded(); err != nil {
-		logger.Log.Errorf("unknown internal while checking if install is needed %s", err)
-		c.AbortWithStatusJSON(500, gin.H{"error": "internal error occured while checking if install is needed"})
-		return
-	} else if !IsInstallNeeded {
-		c.AbortWithStatusJSON(401, gin.H{"error": "cannot register when install is not needed"})
-		return
-	}
-
 	if token, _, err, formErrorMessages := providers.RegisterUser(c.PostForm("username"), c.PostForm("password"), c.Request.Context()); err != nil {
 		if formErrorMessages != nil {
 			logger.Log.Infof("user attempted to register but had form errors %v %v", formErrorMessages, err)
@@ -60,8 +34,6 @@ func Register(c *gin.Context) {
 			c.AbortWithStatusJSON(500, gin.H{"error": "internal error occured while registering"})
 		}
 	} else {
-		c.SetSameSite(http.SameSiteLaxMode)
-		c.SetCookie("x-session-token", *token, 3600, "/", "", false, true)
-		c.JSON(200, "")
+		c.JSON(200, gin.H{"token": token})
 	}
 }
