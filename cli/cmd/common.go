bbb9b783d72d9a8b9fdc01ade33a5762c80fb980
Gustavo Massaneiro <gustavomassa123@gmail.com>
2022-01-24 11:28:02 -0300

    reporting execution time

--- cli/cmd/common.go
+++ cli/cmd/common.go
@@ -35,7 +35,7 @@ func startProxyReportErrorIfAny(kubernetesProvider *kubernetes.Provider, cancel 
 		return
 	}
 
-	apiProvider = apiserver.NewProviderWithoutRetries(GetApiServerUrl(), time.Second)  // short check for proxy
+	apiProvider = apiserver.NewProviderWithoutRetries(GetApiServerUrl(), time.Second) // short check for proxy
 	if err := apiProvider.TestConnection(); err != nil {
 		logger.Log.Debugf("Couldn't connect using proxy, stopping proxy and trying to create port-forward")
 		if err := httpServer.Shutdown(context.Background()); err != nil {
@@ -78,6 +78,8 @@ func handleKubernetesProviderError(err error) {
 
 func finishMizuExecution(kubernetesProvider *kubernetes.Provider, apiProvider *apiserver.Provider, isNsRestrictedMode bool, mizuResourcesNamespace string) {
 	telemetry.ReportAPICalls(apiProvider)
+	telemetry.ReportExecutionTime()
+
 	removalCtx, cancel := context.WithTimeout(context.Background(), cleanupTimeout)
 	defer cancel()
 	dumpLogsIfNeeded(removalCtx, kubernetesProvider)
