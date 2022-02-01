5651499c6747fea21be42ed8e3454e9110ade3cd
Gustavo Massaneiro <gustavomassa123@gmail.com>
2022-02-01 20:43:57 -0300

    added timeout to commands reading tapper output

--- integrationTests/pcap_test.go
+++ integrationTests/pcap_test.go
@@ -1,8 +1,10 @@
 package integrationTests
 
 import (
+	"context"
 	"errors"
 	"fmt"
+	"io"
 	"os"
 	"os/exec"
 	"strings"
@@ -16,6 +18,7 @@ const (
 	BaseninePort            = "9099"
 	BasenineCommandTimeout  = 5 * time.Second
 	APIServerCommandTimeout = 10 * time.Second
+	TestTimeout             = 60 * time.Second
 )
 
 func fileExists(path string) bool {
@@ -41,7 +44,8 @@ func cleanupCommand(cmd *exec.Cmd) error {
 }
 
 func startBasenine(t *testing.T) (*exec.Cmd, string) {
-	basenineCmd := exec.Command("basenine", "-port", BaseninePort)
+	ctx, _ := context.WithTimeout(context.Background(), TestTimeout)
+	basenineCmd := exec.CommandContext(ctx, "basenine", "-port", BaseninePort)
 	t.Logf("running command: %v\n", basenineCmd.String())
 
 	t.Cleanup(func() {
@@ -79,12 +83,14 @@ func startBasenine(t *testing.T) (*exec.Cmd, string) {
 	return nil, ""
 }
 
-func startAPIServer(t *testing.T, configPath string) (*exec.Cmd, string) {
+func startAPIServer(t *testing.T, configPath string) (*exec.Cmd, io.ReadCloser, string) {
 	args := []string{"--api-server"}
 	if len(configPath) > 0 {
 		args = append(args, "--config-path", configPath)
 	}
-	apiServerCmd := exec.Command(AgentBin, args...)
+
+	ctx, _ := context.WithTimeout(context.Background(), TestTimeout)
+	apiServerCmd := exec.CommandContext(ctx, AgentBin, args...)
 	t.Logf("running command: %v", apiServerCmd.String())
 
 	t.Cleanup(func() {
@@ -96,19 +102,19 @@ func startAPIServer(t *testing.T, configPath string) (*exec.Cmd, string) {
 	out, err := apiServerCmd.StderrPipe()
 	if err != nil {
 		t.Errorf("failed to pipe API Server command output: %v", err)
-		return nil, ""
+		return nil, nil, ""
 	}
 
 	if err := apiServerCmd.Start(); err != nil {
 		t.Errorf("failed to start API Server command: %v", err)
-		return nil, ""
+		return nil, nil, ""
 	}
 
 	// wait for some output
 	buff := make([]byte, 32)
 	for stay, timeout := true, time.After(APIServerCommandTimeout); stay; {
 		if n, err := out.Read(buff); err == nil {
-			return apiServerCmd, string(buff[:n])
+			return apiServerCmd, out, string(buff[:n])
 		}
 		select {
 		case <-timeout:
@@ -118,27 +124,28 @@ func startAPIServer(t *testing.T, configPath string) (*exec.Cmd, string) {
 	}
 
 	t.Error("API Server command did not output any data in time")
-	return nil, ""
+	return nil, nil, ""
 }
 
-func startTapper(t *testing.T, pcapPath string) (*exec.Cmd, string) {
+func startTapper(t *testing.T, pcapPath string) (*exec.Cmd, io.ReadCloser, string) {
 	if len(pcapPath) == 0 {
 		t.Error("tapper PCAP file path is empty")
-		return nil, ""
+		return nil, nil, ""
 	}
 
 	if !fileExists(pcapPath) {
 		t.Errorf("tapper PCAP file does not exist: %s", pcapPath)
-		return nil, ""
+		return nil, nil, ""
 	}
 
 	if !strings.HasSuffix(pcapPath, ".cap") {
 		t.Errorf("tapper PCAP file is not a valid .cap file: %s", pcapPath)
-		return nil, ""
+		return nil, nil, ""
 	}
 
-	args := []string{"-r", pcapPath, "--tap", "--api-server-address ws://localhost:8899/wsTapper"}
-	tapperCmd := exec.Command(AgentBin, args...)
+	args := []string{"-r", pcapPath, "--tap", "--api-server-address", "ws://localhost:8899/wsTapper"}
+	ctx, _ := context.WithTimeout(context.Background(), TestTimeout)
+	tapperCmd := exec.CommandContext(ctx, AgentBin, args...)
 	t.Logf("running command: %v", tapperCmd.String())
 
 	t.Cleanup(func() {
@@ -150,19 +157,19 @@ func startTapper(t *testing.T, pcapPath string) (*exec.Cmd, string) {
 	out, err := tapperCmd.StderrPipe()
 	if err != nil {
 		t.Errorf("failed to pipe tapper command output: %v", err)
-		return nil, ""
+		return nil, nil, ""
 	}
 
 	if err := tapperCmd.Start(); err != nil {
 		t.Errorf("failed to start tapper command: %v", err)
-		return nil, ""
+		return nil, nil, ""
 	}
 
 	// wait for some output
 	buff := make([]byte, 32)
 	for stay, timeout := true, time.After(APIServerCommandTimeout); stay; {
 		if n, err := out.Read(buff); err == nil {
-			return tapperCmd, string(buff[:n])
+			return tapperCmd, out, string(buff[:n])
 		}
 		select {
 		case <-timeout:
@@ -172,7 +179,7 @@ func startTapper(t *testing.T, pcapPath string) (*exec.Cmd, string) {
 	}
 
 	t.Error("tapper command did not output any data in time")
-	return nil, ""
+	return nil, nil, ""
 }
 
 func Test(t *testing.T) {
@@ -182,19 +189,47 @@ func Test(t *testing.T) {
 	expectedBasenineOutput := fmt.Sprintf("Listening on :%s\n", BaseninePort)
 	expectedAgentOutput := "Initializing"
 
-	_, output := startBasenine(t)
-
-	if !strings.HasSuffix(output, expectedBasenineOutput) {
-		t.Errorf("basenine is not running as expected - expected: %s, actual: %s", expectedBasenineOutput, output)
+	_, basenineOutput := startBasenine(t)
+	if !strings.HasSuffix(basenineOutput, expectedBasenineOutput) {
+		t.Errorf("basenine is not running as expected - expected: %s, actual: %s", expectedBasenineOutput, basenineOutput)
 	}
 
-	_, output = startAPIServer(t, "")
-	if !strings.HasSuffix(output, expectedAgentOutput) {
-		t.Errorf("API Server is not running as expected - expected: %s, actual: %s", expectedAgentOutput, output)
+	_, tapperOutput, tapperInit := startTapper(t, "http.cap")
+	if !strings.HasSuffix(tapperInit, expectedAgentOutput) {
+		t.Errorf("Tapper is not running as expected - expected: %s, actual: %s", expectedAgentOutput, tapperInit)
 	}
 
-	_, output = startTapper(t, "http.cap")
-	if !strings.HasSuffix(output, expectedAgentOutput) {
-		t.Errorf("Tapper is not running as expected - expected: %s, actual: %s", expectedAgentOutput, output)
+	apiServerCmd, _, apiServerInit := startAPIServer(t, "")
+	if !strings.HasSuffix(apiServerInit, expectedAgentOutput) {
+		t.Errorf("API Server is not running as expected - expected: %s, actual: %s", expectedAgentOutput, apiServerInit)
+	}
+
+	resultChannel := make(chan error, 1)
+
+	go func() {
+		// lets give x seconds to read the output we need from the tapper
+		buff := make([]byte, 1024)
+		for stay, timeout := true, time.After(APIServerCommandTimeout); stay; {
+			if n, err := tapperOutput.Read(buff); err == nil {
+				fmt.Println(string(buff[:n]))
+			}
+			select {
+			case <-timeout:
+				stay = false
+			default:
+			}
+		}
+
+		if err := apiServerCmd.Wait(); err != nil {
+			resultChannel <- err
+			return
+		}
+
+		resultChannel <- nil
+	}()
+
+	testResult := <-resultChannel
+	if testResult != nil {
+		t.Errorf("unexpected result: %v", testResult)
 	}
 }
