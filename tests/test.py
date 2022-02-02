be7deefe720b695ebdc5f86c7004c2e80842227c
M. Mert Yildiran <mehmet@up9.com>
2022-02-02 18:13:43 +0300

    Add a Python script to automate the PCAP testbed

---
+++ tests/test.py
@@ -0,0 +1,112 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+"""
+.. module:: test
+    :synopsis: Automation for the PCAP testbed
+"""
+
+import _thread
+import time
+import json
+import sys
+from functools import partial
+from typing import List
+
+import websocket
+import jsonpickle
+import requests
+
+
+HOST = 'localhost'
+PORT = '8899'
+WEBSOCKET_TIMEOUT = 3
+ENTRIES_ENDPOINT = 'http://%s:%s/entries' % (HOST, PORT)
+WEBSOCKET_ENDPOINT = 'ws://%s:%s/ws' % (HOST, PORT)
+
+
+class Entry:
+
+    def __init__(self, _id: int, base: dict) -> None:
+        self.id = _id # type: int
+        self.base = base # type: dict
+        self.data = {} # type: dict
+
+    def set_data(self, data: dict) -> None:
+        self.data = data
+
+class DataGroup:
+
+    def __init__(self, query: str) -> None:
+        self.query = query # type: str
+        self.entries = [] # type: List[Entry]
+
+
+class Suite:
+
+    def __init__(self) -> None:
+        self.data_groups = [] # type: List[DataGroup]
+
+
+queries = [
+    '',
+    # 'method == "PING"',
+    # 'method == "FLUSHDB"',
+]
+
+current_data_group = None # type: List[DataGroup]
+
+def on_message(ws, message):
+    global current_data_group
+    data = json.loads(message)
+    if data['messageType'] == 'entry':
+        entry = Entry(_id=data['data']['id'], base=data)
+        current_data_group.entries.append(entry)
+
+def on_error(ws, error):
+    print(error)
+
+def on_close(ws, close_status_code, close_msg):
+    print('WebSocket closed.')
+
+def on_open(ws, query):
+    def run(*args):
+        ws.send(query) # query
+        time.sleep(WEBSOCKET_TIMEOUT)
+        ws.close()
+        print('thread terminating...')
+    _thread.start_new_thread(run, ())
+
+
+if __name__ == "__main__":
+    suite = Suite()
+    # websocket.enableTrace(True)
+    for query in queries:
+        print('Running query "%s"...' % query)
+        data_group = DataGroup(query=query)
+        suite.data_groups.append(data_group)
+        current_data_group = data_group
+        on_open_extended = partial(on_open, query=query)
+        ws = websocket.WebSocketApp(
+            WEBSOCKET_ENDPOINT,
+            on_open=on_open_extended,
+            on_message=on_message,
+            on_error=on_error,
+            on_close=on_close
+        )
+        ws.run_forever()
+
+        print('Streamed %d entries.' % len(data_group.entries))
+
+    for data_group in suite.data_groups:
+        print('Query: "%s" Fetching full entries...' % data_group.query)
+        for i, entry in enumerate(data_group.entries):
+            sys.stdout.write("Fetch progress: %d/%d \r" % (i, len(data_group.entries)))
+            url = '%s/%d' % (ENTRIES_ENDPOINT, entry.id)
+            resp = requests.get(url=url, params={'query': data_group.query})
+            data = resp.json()
+            entry.set_data(data)
+
+    # Print everything
+    serialized = jsonpickle.encode(suite)
+    print(json.dumps(json.loads(serialized), indent=2))
