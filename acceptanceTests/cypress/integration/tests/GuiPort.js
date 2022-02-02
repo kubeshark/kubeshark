acee390998c585c3b6c7062f942172e51c79b9ac
Adam Kol <adam@up9.com>
2022-02-02 15:37:27 +0200

    no message

--- acceptanceTests/cypress/integration/tests/GuiPort.js
+++ acceptanceTests/cypress/integration/tests/GuiPort.js
@@ -3,10 +3,11 @@ import {verifyMinimumEntries} from "../testHelpers/TrafficHelper";
 
 it('check', function () {
     const podName = Cypress.env('name'), namespace = Cypress.env('namespace');
-    cy.intercept('GET', `http://localhost:${Cypress.env('port')}/status/tap`).as('statusTap');
+    const port = Cypress.env('port');
+    cy.intercept('GET', `http://localhost:${port}/status/tap`).as('statusTap');
 
-    cy.visit(`http://localhost:${Cypress.env('port')}`);
-    cy.wait('@statusTap').its('response.statusCode').should('match', /^2\d{2}/)
+    cy.visit(`http://localhost:${port}`);
+    cy.wait('@statusTap').its('response.statusCode').should('match', /^2\d{2}/);
 
     verifyMinimumEntries();
 
