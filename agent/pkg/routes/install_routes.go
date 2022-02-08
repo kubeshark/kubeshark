8660091f230fd1aacb7f40e647a6458cb9581a5e
Rami Berman <rami.berman@up9.com>
2022-02-08 16:37:21 +0200

    Update config_routes.go, entries_routes.go, and 7 more files...

--- agent/pkg/routes/install_routes.go
+++ agent/pkg/routes/install_routes.go
@@ -6,16 +6,9 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-var (
-	InstallGetIsNeededHandler = controllers.IsSetupNecessary
-	InstallPostAdminHandler   = controllers.SetupAdminUser
-)
-
-func InstallRoutes(ginApp *gin.Engine) *gin.RouterGroup {
+func InstallRoutes(ginApp *gin.Engine) {
 	routeGroup := ginApp.Group("/install")
 
-	routeGroup.GET("/isNeeded", func(c *gin.Context) { InstallGetIsNeededHandler(c) })
-	routeGroup.POST("/admin", func(c *gin.Context) { InstallPostAdminHandler(c) })
-
-	return routeGroup
+	routeGroup.GET("/isNeeded", controllers.IsSetupNecessary)
+	routeGroup.POST("/admin", controllers.SetupAdminUser)
 }
--- agent/pkg/routes/status_routes.go
+++ agent/pkg/routes/status_routes.go
@@ -7,39 +7,24 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-var (
-	StatusGetHealthCheck                 = controllers.HealthCheck
-	StatusPostTappedPods                 = controllers.PostTappedPods
-	StatusPostTapperStatus               = controllers.PostTapperStatus
-	StatusGetConnectedTappersCount       = controllers.GetConnectedTappersCount
-	StatusGetTappingStatus               = controllers.GetTappingStatus
-	StatusGetAuthStatus                  = controllers.GetAuthStatus
-	StatusGetAnalyzeInformation          = controllers.AnalyzeInformation
-	StatusGetGeneralStats                = controllers.GetGeneralStats
-	StatusGetRecentTLSLinks              = controllers.GetRecentTLSLinks
-	StatusGetCurrentResolvingInformation = controllers.GetCurrentResolvingInformation
-)
-
-func StatusRoutes(ginApp *gin.Engine) *gin.RouterGroup {
+func StatusRoutes(ginApp *gin.Engine) {
 	routeGroup := ginApp.Group("/status")
 	routeGroup.Use(middlewares.RequiresAuth())
 
-	routeGroup.GET("/health", func(c *gin.Context) { StatusGetHealthCheck(c) })
+	routeGroup.GET("/health", controllers.HealthCheck)
 
-	routeGroup.POST("/tappedPods", func(c *gin.Context) { StatusPostTappedPods(c) })
-	routeGroup.POST("/tapperStatus", func(c *gin.Context) { StatusPostTapperStatus(c) })
-	routeGroup.GET("/connectedTappersCount", func(c *gin.Context) { StatusGetConnectedTappersCount(c) })
-	routeGroup.GET("/tap", func(c *gin.Context) { StatusGetTappingStatus(c) })
+	routeGroup.POST("/tappedPods", controllers.PostTappedPods)
+	routeGroup.POST("/tapperStatus", controllers.PostTapperStatus)
+	routeGroup.GET("/connectedTappersCount", controllers.GetConnectedTappersCount)
+	routeGroup.GET("/tap", controllers.GetTappingStatus)
 
-	routeGroup.GET("/auth", func(c *gin.Context) { StatusGetAuthStatus(c) })
+	routeGroup.GET("/auth", controllers.GetAuthStatus)
 
-	routeGroup.GET("/analyze", func(c *gin.Context) { StatusGetAnalyzeInformation(c) })
+	routeGroup.GET("/analyze", controllers.AnalyzeInformation)
 
-	routeGroup.GET("/general", func(c *gin.Context) { StatusGetGeneralStats(c) }) // get general stats about entries in DB
+	routeGroup.GET("/general", controllers.GetGeneralStats) // get general stats about entries in DB
 
-	routeGroup.GET("/recentTLSLinks", func(c *gin.Context) { StatusGetRecentTLSLinks(c) })
+	routeGroup.GET("/recentTLSLinks", controllers.GetRecentTLSLinks)
 
-	routeGroup.GET("/resolving", func(c *gin.Context) { StatusGetCurrentResolvingInformation(c) })
-
-	return routeGroup
+	routeGroup.GET("/resolving", controllers.GetCurrentResolvingInformation)
 }
