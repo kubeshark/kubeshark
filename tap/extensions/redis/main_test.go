418f89375c63a8c8a63e2396f97dad6f6c5f4823
M. Mert Yildiran <mehmet@up9.com>
2022-02-15 15:38:10 +0300

    Fix `dissector.Analyze` call

--- tap/extensions/redis/main_test.go
+++ tap/extensions/redis/main_test.go
@@ -204,7 +204,7 @@ func TestAnalyze(t *testing.T) {
 
 		var entries []*api.Entry
 		for _, item := range items {
-			entry := dissector.Analyze(item, "", "")
+			entry := dissector.Analyze(item, "", "", "")
 			entries = append(entries, entry)
 		}
 
