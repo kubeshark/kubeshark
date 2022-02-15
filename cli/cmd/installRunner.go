8ab2122396eb436a4cfa92ce51258f72fd56557f
Igor Gov <iggvrv@gmail.com>
2022-02-15 10:57:14 +0200

    Sending telemetry config to server

--- cli/cmd/installRunner.go
+++ cli/cmd/installRunner.go
@@ -74,6 +74,7 @@ func getInstallMizuAgentConfig(maxDBSizeBytes int64, tapperResources shared.Reso
 		StandaloneMode:         true,
 		ServiceMap:             config.Config.ServiceMap,
 		OAS:                    config.Config.OAS,
+		Telemetry:              config.Config.Telemetry,
 		Elastic:                config.Config.Elastic,
 	}
 
