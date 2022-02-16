4c8622911aac042d0fb8122b36e269d68dc4a500
David Levanon <dvdlevanon@gmail.com>
2022-02-16 13:43:16 +0200

    move to alpine, use local copy of mizu instead of git, add readme

--- tap/tlstapper/bpf-builder/build.sh
+++ tap/tlstapper/bpf-builder/build.sh
@@ -1,22 +1,11 @@
 #!/bin/bash
 
-MIZU_BRANCH=$1
-[ -z $MIZU_BRANCH ] && MIZU_BRANCH="develop"
+MIZU_HOME=$(realpath ../../../)
 
-docker build --build-arg "MIZU_BRANCH=$MIZU_BRANCH" -t mizu-ebpf-builder . || exit 1
-
-mkdir -p output
+docker build -t mizu-ebpf-builder . || exit 1
 
 docker run --rm \
 	--name mizu-ebpf-builder \
-	-v /usr/include/:/usr/include \
-	-v $(pwd)/output:/output \
+	-v $MIZU_HOME:/mizu \
 	-it mizu-ebpf-builder \
-	bash -c "
-		[ ! -r tap/tlstapper/tls_tapper.go ] && { echo File not found tap/tlstapper/tls_tapper.go; exit 1; }
-		go generate tap/tlstapper/tls_tapper.go
-		cp tap/tlstapper/tlstapper_bpfeb.o /output
-		cp tap/tlstapper/tlstapper_bpfel.o /output
-	" || exit 1
-
-cp output/*.o ../
+	sh -c "go generate tap/tlstapper/tls_tapper.go" || exit 1
