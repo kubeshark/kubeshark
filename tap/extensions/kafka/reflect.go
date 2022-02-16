c67675c1386d5197b0ec50aad8cd1b7c58139ac1
M. Mert Yıldıran <mehmet@up9.com>
2022-02-16 12:18:33 +0300

    Add unit tests for Kafka dissector (#807)
    * Add unit tests for Kafka dissector
    
    * Return `io.EOF` if request or response header size is zero
    
    * Sort the slice in `representMapAsTable`
    
    * Remove the dead code
    
    * Remove more dead code
    
    * Remove more dead code
    
    * Fix `dissector.Analyze` call
    
    Co-authored-by: gadotroee <55343099+gadotroee@users.noreply.github.com>

--- tap/extensions/kafka/reflect.go
+++ tap/extensions/kafka/reflect.go
@@ -8,45 +8,13 @@ import (
 
 type index []int
 
-type _type struct{ typ reflect.Type }
-
-func typeOf(x interface{}) _type {
-	return makeType(reflect.TypeOf(x))
-}
-
-func elemTypeOf(x interface{}) _type {
-	return makeType(reflect.TypeOf(x).Elem())
-}
-
-func makeType(t reflect.Type) _type {
-	return _type{typ: t}
-}
-
 type value struct {
 	val reflect.Value
-}
-
-func nonAddressableValueOf(x interface{}) value {
-	return value{val: reflect.ValueOf(x)}
 }
 
 func valueOf(x interface{}) value {
 	return value{val: reflect.ValueOf(x).Elem()}
 }
-
-func (v value) bool() bool { return v.val.Bool() }
-
-func (v value) int8() int8 { return int8(v.int64()) }
-
-func (v value) int16() int16 { return int16(v.int64()) }
-
-func (v value) int32() int32 { return int32(v.int64()) }
-
-func (v value) int64() int64 { return v.val.Int() }
-
-func (v value) string() string { return v.val.String() }
-
-func (v value) bytes() []byte { return v.val.Bytes() }
 
 func (v value) iface(t reflect.Type) interface{} { return v.val.Addr().Interface() }
 
@@ -87,10 +55,6 @@ func makeArray(t reflect.Type, n int) array {
 }
 
 func (a array) index(i int) value { return value{val: a.val.Index(i)} }
-
-func (a array) length() int { return a.val.Len() }
-
-func (a array) isNil() bool { return a.val.IsNil() }
 
 func indexOf(s reflect.StructField) index { return index(s.Index) }
 
