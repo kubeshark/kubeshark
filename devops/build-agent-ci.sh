ac7d24d01a83fea4af62e6020dc0b437a18293c5
Nimrod Gilboa Markevich <nimrod@up9.com>
2022-02-23 12:27:21 +0200

    Revert "tmp: Pull docker image before build"
    This reverts commit a39415f7e4c9354f1b399351c32bd1cf68148b31.

--- devops/build-agent-ci.sh
+++ devops/build-agent-ci.sh
@@ -11,9 +11,5 @@ VER=${VER=0.0}
 
 DOCKER_TAGGED_BUILD="$DOCKER_REPO:$VER"
 
-echo "pulling agent docker images"
-docker pull gcr.io/up9-docker-hub/mizu/develop:28.0-dev14-arm64v8
-docker pull gcr.io/up9-docker-hub/mizu/develop:28.0-dev14-amd64
-
 echo "building $DOCKER_TAGGED_BUILD"
 docker build -t ${DOCKER_TAGGED_BUILD} --build-arg VER=${VER} --build-arg BUILD_TIMESTAMP=${BUILD_TIMESTAMP} --build-arg GIT_BRANCH=${GIT_BRANCH} --build-arg COMMIT_HASH=${COMMIT_HASH} .
