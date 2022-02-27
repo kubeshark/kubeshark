b53399d5286a025d918464489ae83a3559d6e0eb
Nimrod Gilboa Markevich <nimrod@up9.com>
2022-02-27 17:47:17 +0200

    Rename variable in script because USERNAME is a special variable in zsh

--- acceptanceTests/create_user.sh
+++ acceptanceTests/create_user.sh
@@ -7,14 +7,14 @@
 
 set -e
 
-USERNAME=$1
+NEW_USERNAME=$1
 CERT_DIR="${HOME}/certs"
-KEY_FILE="${CERT_DIR}/${USERNAME}.key"
-CRT_FILE="${CERT_DIR}/${USERNAME}.crt"
+KEY_FILE="${CERT_DIR}/${NEW_USERNAME}.key"
+CRT_FILE="${CERT_DIR}/${NEW_USERNAME}.crt"
 MINIKUBE_KEY_FILE="${HOME}/.minikube/ca.key"
 MINIKUBE_CRT_FILE="${HOME}/.minikube/ca.crt"
 
-echo "Creating user and context for username \"${USERNAME}\" in Minikube cluster"
+echo "Creating user and context for username \"${NEW_USERNAME}\" in Minikube cluster"
 
 if ! command -v openssl &> /dev/null
 then
@@ -23,14 +23,14 @@ then
 	 sudo apt-get install openssl
 fi
 
-echo "Creating certificate for user \"${USERNAME}\""
+echo "Creating certificate for user \"${NEW_USERNAME}\""
 mkdir -p ${CERT_DIR}
 echo "Generating key \"${KEY_FILE}\""
 openssl genrsa -out "${KEY_FILE}" 2048
 echo "Generating crt \"${CRT_FILE}\""
-openssl req -new -key "${KEY_FILE}" -out "${CRT_FILE}" -subj "/CN=${USERNAME}/O=group1"
+openssl req -new -key "${KEY_FILE}" -out "${CRT_FILE}" -subj "/CN=${NEW_USERNAME}/O=group1"
 openssl x509 -req -in "${CRT_FILE}" -CA "${MINIKUBE_CRT_FILE}" -CAkey "${MINIKUBE_KEY_FILE}" -CAcreateserial -out "${CRT_FILE}" -days 500
 
-echo "Creating context for user \"${USERNAME}\""
-kubectl config set-credentials "${USERNAME}" --client-certificate="${CRT_FILE}" --client-key="${KEY_FILE}"
-kubectl config set-context "${USERNAME}" --cluster=minikube --user="${USERNAME}"
+echo "Creating context for user \"${NEW_USERNAME}\""
+kubectl config set-credentials "${NEW_USERNAME}" --client-certificate="${CRT_FILE}" --client-key="${KEY_FILE}"
+kubectl config set-context "${NEW_USERNAME}" --cluster=minikube --user="${NEW_USERNAME}"
