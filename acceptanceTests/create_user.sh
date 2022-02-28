a02a4f04c056b83a6e73eac4b0c4e3002fe4f157
Nimrod Gilboa Markevich <nimrod@up9.com>
2022-02-28 11:00:43 +0200

    Reduce number of days for the certificate

--- acceptanceTests/create_user.sh
+++ acceptanceTests/create_user.sh
@@ -13,6 +13,7 @@ KEY_FILE="${CERT_DIR}/${NEW_USERNAME}.key"
 CRT_FILE="${CERT_DIR}/${NEW_USERNAME}.crt"
 MINIKUBE_KEY_FILE="${HOME}/.minikube/ca.key"
 MINIKUBE_CRT_FILE="${HOME}/.minikube/ca.crt"
+DAYS=1
 
 echo "Creating user and context for username \"${NEW_USERNAME}\" in Minikube cluster"
 
@@ -29,7 +30,7 @@ echo "Generating key \"${KEY_FILE}\""
 openssl genrsa -out "${KEY_FILE}" 2048
 echo "Generating crt \"${CRT_FILE}\""
 openssl req -new -key "${KEY_FILE}" -out "${CRT_FILE}" -subj "/CN=${NEW_USERNAME}/O=group1"
-openssl x509 -req -in "${CRT_FILE}" -CA "${MINIKUBE_CRT_FILE}" -CAkey "${MINIKUBE_KEY_FILE}" -CAcreateserial -out "${CRT_FILE}" -days 500
+openssl x509 -req -in "${CRT_FILE}" -CA "${MINIKUBE_CRT_FILE}" -CAkey "${MINIKUBE_KEY_FILE}" -CAcreateserial -out "${CRT_FILE}" -days $DAYS
 
 echo "Creating context for user \"${NEW_USERNAME}\""
 kubectl config set-credentials "${NEW_USERNAME}" --client-certificate="${CRT_FILE}" --client-key="${KEY_FILE}"
