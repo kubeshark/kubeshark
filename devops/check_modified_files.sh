7929bd0583be792793b31be5ca1f22e25e996141
Igor Gov <iggvrv@gmail.com>
2022-03-06 13:11:42 +0200

    modified files script var renaming

--- devops/check_modified_files.sh
+++ devops/check_modified_files.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-folders_arr=( "$@" )
+paths_arr=( "$@" )
 
 printf "\n========== List modified files ==========\n"
 echo "$(git diff --name-only HEAD^ HEAD)"
 
 printf "\n========== List paths to match and check existence ==========\n"
-for folder in ${folders_arr[*]}
+for path in ${paths_arr[*]}
 do
-  if [ -f "$folder" ] || [ -d "$folder" ]; then
-      echo "$folder - found"
+  if [ -f "$path" ] || [ -d "$path" ]; then
+      echo "$path - found"
   else
-      echo "$folder - does not found - exiting with failure"
+      echo "$path - does not found - exiting with failure"
       exit 1
   fi
 done
@@ -20,10 +20,10 @@ git diff --name-only HEAD^ HEAD > files.txt
 matched=false
 while IFS= read -r file
 do
-  for folder in ${folders_arr[*]}
+  for path in ${paths_arr[*]}
   do
-      if [[ $file == $folder* ]]; then
-        echo "$file - match path: $folder"
+      if [[ $file == $path* ]]; then
+        echo "$file - match path: $path"
         matched=true
         break
       fi
