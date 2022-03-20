59ac006a5659e1d4ac7b2f5fb6245bc42b817cba
Nimrod Gilboa Markevich <nimrod@up9.com>
2022-03-20 13:52:40 +0200

    Add function for broadcasting tapped pods to from server to tappers

--- agent/pkg/api/utils.go
+++ agent/pkg/api/utils.go
@@ -2,6 +2,7 @@ package api
 
 import (
 	"encoding/json"
+	core "k8s.io/api/core/v1"
 
 	"github.com/up9inc/mizu/agent/pkg/providers/tappedPods"
 	"github.com/up9inc/mizu/shared"
@@ -16,5 +17,14 @@ func BroadcastTappedPodsStatus() {
 		logger.Log.Errorf("Could not Marshal message %v", err)
 	} else {
 		BroadcastToBrowserClients(jsonBytes)
+	}
+}
+
+func BroadcastTappedPodsToTappers(nodeToTappedPodMap map[string][]core.Pod) {
+	message := shared.CreateWebSocketTappedPodsMessage(nodeToTappedPodMap)
+	if jsonBytes, err := json.Marshal(message); err != nil {
+		logger.Log.Errorf("Could not Marshal message %v", err)
+	} else {
+		BroadcastToTapperClients(jsonBytes)
 	}
 }
