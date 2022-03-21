9a175fe1716b23aca241332a3db672a36ddeb5a7
Adam Kol <adam@up9.com>
2022-03-21 15:48:57 +0200

    closing ws on modal open

--- ui-common/src/components/TrafficViewer/TrafficViewerApi.ts
+++ ui-common/src/components/TrafficViewer/TrafficViewerApi.ts
@@ -1,13 +1,13 @@
 type TrafficViewerApi = {
-    validateQuery : (query: any) => any
-    tapStatus : () => any
-    analyzeStatus : () => any
-    fetchEntries : (leftOff: any, direction: number, query: any, limit: number, timeoutMs: number) => any
-    getEntry : (entryId : any, query:string) => any
-    getRecentTLSLinks : () => any,
-    webSocket : {
-      close : () => {}
-    }
+  validateQuery: (query: any) => any
+  tapStatus: () => any
+  analyzeStatus: () => any
+  fetchEntries: (leftOff: any, direction: number, query: any, limit: number, timeoutMs: number) => any
+  getEntry: (entryId: any, query: string) => any
+  getRecentTLSLinks: () => any,
+  webSocket: {
+    close: () => void
   }
+}
 
-  export default TrafficViewerApi
+export default TrafficViewerApi
--- ui/src/components/Pages/TrafficPage/TrafficPage.tsx
+++ ui/src/components/Pages/TrafficPage/TrafficPage.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
 import { Button } from "@material-ui/core";
 import Api,{getWebsocketUrl} from "../../../helpers/api";
 import debounce from 'lodash/debounce';
@@ -8,8 +8,8 @@ import serviceMapModalOpenAtom from "../../../recoil/serviceMapModalOpen";
 import TrafficViewer from "@up9/mizu-common"
 import "@up9/mizu-common/dist/index.css"
 import oasModalOpenAtom from "../../../recoil/oasModalOpen/atom";
-import serviceMap from "../../assets/serviceMap.svg";	
-import services from "../../assets/services.svg";	
+import serviceMap from "../../assets/serviceMap.svg";
+import services from "../../assets/services.svg";
 
 interface TrafficPageProps {
   setAnalyzeStatus?: (status: any) => void;
@@ -21,38 +21,40 @@ export const TrafficPage: React.FC<TrafficPageProps> = ({setAnalyzeStatus}) => {
   const commonClasses = useCommonStyles();
   const setServiceMapModalOpen = useSetRecoilState(serviceMapModalOpenAtom);
   const [openOasModal, setOpenOasModal] = useRecoilState(oasModalOpenAtom);
+  const [openWebSocket, setOpenWebSocket] = useState(true);
 
 const trafficViewerApi = {...api}
 
-  const handleOpenOasModal = () => {	
-    //closeSocket() -- Todo: Add Close webSocket
-    setOpenOasModal(true);	
+  const handleOpenOasModal = () => {
+    setOpenWebSocket(false)
+    setOpenOasModal(true);
   }
 
   const openServiceMapModalDebounce = debounce(() => {
+    setOpenWebSocket(false)
     setServiceMapModalOpen(true)
   }, 500);
 
-  const actionButtons = (window["isOasEnabled"] || window["isServiceMapEnabled"]) && 
-                          <div style={{ display: 'flex', height: "100%" }}>	
-                              {window["isOasEnabled"] && <Button	
-                                startIcon={<img className="custom" src={services} alt="services"></img>}	
-                                size="large"	
-                                type="submit"	
-                                variant="contained"	
-                                className={commonClasses.outlinedButton + " " + commonClasses.imagedButton}	
-                                style={{ marginRight: 25 }}	
-                                onClick={handleOpenOasModal}>	
-                                Show OAS	
-                              </Button>}	
-                              {window["isServiceMapEnabled"] && <Button	
-                                startIcon={<img src={serviceMap} className="custom" alt="service-map" style={{marginRight:"8%"}}></img>}	
-                                size="large"	
-                                variant="contained"	
-                                className={commonClasses.outlinedButton + " " + commonClasses.imagedButton}	
-                                onClick={openServiceMapModalDebounce}>	
-                                Service Map	
-                              </Button>}	
+  const actionButtons = (window["isOasEnabled"] || window["isServiceMapEnabled"]) &&
+                          <div style={{ display: 'flex', height: "100%" }}>
+                              {window["isOasEnabled"] && <Button
+                                startIcon={<img className="custom" src={services} alt="services"></img>}
+                                size="large"
+                                type="submit"
+                                variant="contained"
+                                className={commonClasses.outlinedButton + " " + commonClasses.imagedButton}
+                                style={{ marginRight: 25 }}
+                                onClick={handleOpenOasModal}>
+                                Show OAS
+                              </Button>}
+                              {window["isServiceMapEnabled"] && <Button
+                                startIcon={<img src={serviceMap} className="custom" alt="service-map" style={{marginRight:"8%"}}></img>}
+                                size="large"
+                                variant="contained"
+                                className={commonClasses.outlinedButton + " " + commonClasses.imagedButton}
+                                onClick={openServiceMapModalDebounce}>
+                                Service Map
+                              </Button>}
                         </div>
 
   useEffect(() => {
@@ -61,9 +63,9 @@ const trafficViewerApi = {...api}
     }
   },[])
 
-  return ( 
+  return (
   <>
-      <TrafficViewer setAnalyzeStatus={setAnalyzeStatus} webSocketUrl={getWebsocketUrl()}
+      <TrafficViewer setAnalyzeStatus={setAnalyzeStatus} webSocketUrl={getWebsocketUrl()} isCloseWebSocket={!openWebSocket}
                      trafficViewerApiProp={trafficViewerApi} actionButtons={actionButtons} isShowStatusBar={!openOasModal}/>
   </>
   );
