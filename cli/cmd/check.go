cff5987ed477050c09fc3a03162bd09be746f3da
RoyUP9 <87927115+RoyUP9@users.noreply.github.com>
2022-03-21 17:19:04 +0200

    Added check pre install (#905)

--- cli/cmd/check.go
+++ cli/cmd/check.go
@@ -27,5 +27,6 @@ func init() {
 	}
 
 	checkCmd.Flags().Bool(configStructs.PreTapCheckName, defaultCheckConfig.PreTap, "Check pre-tap Mizu installation for potential problems")
+	checkCmd.Flags().Bool(configStructs.PreInstallCheckName, defaultCheckConfig.PreInstall, "Check pre-install Mizu installation for potential problems")
 	checkCmd.Flags().Bool(configStructs.ImagePullCheckName, defaultCheckConfig.ImagePull, "Test connectivity to container image registry by creating and removing a temporary pod in 'default' namespace")
 }
