b746090601c8e27d65070dec3225a8e0981a8c73
Nimrod Gilboa Markevich <59927337+nimrod-up9@users.noreply.github.com>
2022-03-22 15:15:36 +0200

    Print pod names in the k8s idiomatic namespace/pod
    Co-authored-by: gadotroee <55343099+gadotroee@users.noreply.github.com>

--- shared/models.go
+++ shared/models.go
@@ -92,7 +92,7 @@ func (np NodeToPodsMap) Summary() map[string][]string {
 	summary := make(map[string][]string)
 	for node, pods := range np {
 		for _, pod := range pods {
-			summary[node] = append(summary[node], pod.Name + "." + pod.Namespace)
+			summary[node] = append(summary[node], pod.Namespace + "/" +  pod.Name)
 		}
 	}
 
