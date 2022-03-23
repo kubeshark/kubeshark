a17e922411c4a16348581c882f1661508101c552
Leon <>
2022-03-23 13:00:30 +0200

    icon changed

--- ui-common/src/components/UI/InformationIcon.tsx
+++ ui-common/src/components/UI/InformationIcon.tsx
@@ -1,16 +1,18 @@
-import React from "react";
+import React, { CSSProperties } from "react";
 import infoImg from 'assets/info.svg';
+import styles from "./style/InformationIcon.module.sass"
 
 const DEFUALT_LINK = "https://getmizu.io/docs"
 
 export interface InformationIconProps{
-    link?: string
+    link?: string,
+    style? : CSSProperties
 }
 
-export const InformationIcon: React.FC<InformationIconProps> = ({link}) => {
-
+export const InformationIcon: React.FC<InformationIconProps> = ({link,style}) => {
     return <React.Fragment>
-        <a href={DEFUALT_LINK ? DEFUALT_LINK : link}>
+        
+        <a href={DEFUALT_LINK ? DEFUALT_LINK : link} style={style} className={styles.flex}>
             <img className="headerIcon"  src={infoImg} alt="Info icon"/>
         </a>
     </React.Fragment>
