3aa19ce58ff2abda636ac2e16feb77510250b260
Leon <>
2022-03-23 13:15:55 +0200

    space removed

--- ui/src/components/Header/Header.tsx
+++ ui/src/components/Header/Header.tsx
@@ -14,7 +14,7 @@ export const Header: React.FC<HeaderProps> = ({analyzeStatus}) => {
             <div className="title"><img src={logo} alt="logo"/></div>
             <div className="description">Traffic viewer for Kubernetes</div>
         </div>
-        <div style={{display: "flex", alignItems: "center"}}>       
+        <div style={{display: "flex", alignItems: "center"}}>
             {analyzeStatus?.isAnalyzing && <AnalyzeButton analyzeStatus={analyzeStatus}/>}
             <UI.InformationIcon/>
             <AuthPresentation/>
