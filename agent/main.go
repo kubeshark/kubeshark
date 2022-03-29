63cf7ac34e8f85495c61d4c197a2c3047fe8f79a
RamiBerm <54766858+RamiBerm@users.noreply.github.com>
2022-03-29 18:30:19 +0300

    Refactor entries controller logic (#949)
    * wip
    
    * Update entries_controller.go and entries_provider.go
    
    * Update entries_controller.go
    
    * change entries provider into a struct + interface
    
    * Update entries_provider.go
    
    * Update entries_provider.go

--- agent/main.go
+++ agent/main.go
@@ -18,6 +18,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/up9inc/mizu/agent/pkg/dependency"
 	"github.com/up9inc/mizu/agent/pkg/elastic"
+	"github.com/up9inc/mizu/agent/pkg/entries"
 	"github.com/up9inc/mizu/agent/pkg/middlewares"
 	"github.com/up9inc/mizu/agent/pkg/models"
 	"github.com/up9inc/mizu/agent/pkg/oas"
@@ -371,4 +372,5 @@ func handleIncomingMessageAsTapper(socketConnection *websocket.Conn) {
 func initializeDependencies() {
 	dependency.RegisterGenerator(dependency.ServiceMapGeneratorDependency, func() interface{} { return servicemap.GetDefaultServiceMapInstance() })
 	dependency.RegisterGenerator(dependency.OasGeneratorDependency, func() interface{} { return oas.GetDefaultOasGeneratorInstance() })
+	dependency.RegisterGenerator(dependency.EntriesProvider, func() interface{} { return &entries.BasenineEntriesProvider{} })
 }
