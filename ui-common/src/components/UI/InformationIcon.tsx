ce26731ef3962a6ce2730a4ddcde7717a51112e5
Leon <>
2022-04-03 14:34:03 +0300

    Docs icon change Hide Navbar on serviceMap modal open

--- ui-common/src/components/UI/InformationIcon.tsx
+++ ui-common/src/components/UI/InformationIcon.tsx
@@ -4,15 +4,15 @@ import styles from "./style/InformationIcon.module.sass"
 
 const DEFUALT_LINK = "https://getmizu.io/docs"
 
-export interface InformationIconProps{
+export interface InformationIconProps {
     link?: string,
-    style? : CSSProperties
+    style?: CSSProperties
 }
 
-export const InformationIcon: React.FC<InformationIconProps> = ({link,style}) => {
+export const InformationIcon: React.FC<InformationIconProps> = ({ link, style }) => {
     return <React.Fragment>
-        <a href={DEFUALT_LINK ? DEFUALT_LINK : link} style={style} className={styles.flex} title="documentation" target="_blank">
-            <img className="headerIcon"  src={infoImg} alt="Info icon"/>
+        <a href={DEFUALT_LINK ? DEFUALT_LINK : link} style={style} className={styles.linkStyle} title="documentation" target="_blank">
+            <span >Docs</span>
         </a>
     </React.Fragment>
 }
