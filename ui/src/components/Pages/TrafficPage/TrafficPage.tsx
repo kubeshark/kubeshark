7adbf7bf1b1a64340ac0105d6a190edcd99efb64
leon-up9 <97597983+leon-up9@users.noreply.github.com>
2022-04-04 14:49:41 +0300

    Ui/TRA-4461_service-map-&-OAS---GUI-changes (#962)
    * OpenAPI renamed to Service Catalog
    
    * Docs icon change
    Hide Navbar on serviceMap modal open
    
    * PR comments
    
    Co-authored-by: Leon <>
    Co-authored-by: RoyUP9 <87927115+RoyUP9@users.noreply.github.com>

--- ui/src/components/Pages/TrafficPage/TrafficPage.tsx
+++ ui/src/components/Pages/TrafficPage/TrafficPage.tsx
@@ -1,9 +1,9 @@
-import React, {useEffect, useState} from "react";
+import React, { useState } from "react";
 import { Button } from "@material-ui/core";
 import Api, { MizuWebsocketURL } from "../../../helpers/api";
 import debounce from 'lodash/debounce';
-import {useSetRecoilState, useRecoilState} from "recoil";
-import {useCommonStyles} from "../../../helpers/commonStyle"
+import { useRecoilState } from "recoil";
+import { useCommonStyles } from "../../../helpers/commonStyle"
 import serviceMapModalOpenAtom from "../../../recoil/serviceMapModalOpen";
 import TrafficViewer from "@up9/mizu-common"
 import "@up9/mizu-common/dist/index.css"
@@ -17,13 +17,13 @@ interface TrafficPageProps {
 
 const api = Api.getInstance();
 
-export const TrafficPage: React.FC<TrafficPageProps> = ({setAnalyzeStatus}) => {
+export const TrafficPage: React.FC<TrafficPageProps> = ({ setAnalyzeStatus }) => {
   const commonClasses = useCommonStyles();
-  const setServiceMapModalOpen = useSetRecoilState(serviceMapModalOpenAtom);
+  const [serviceMapModalOpen, setServiceMapModalOpen] = useRecoilState(serviceMapModalOpenAtom);
   const [openOasModal, setOpenOasModal] = useRecoilState(oasModalOpenAtom);
   const [openWebSocket, setOpenWebSocket] = useState(true);
 
-const trafficViewerApi = {...api}
+  const trafficViewerApi = { ...api }
 
   const handleOpenOasModal = () => {
     setOpenWebSocket(false)
@@ -36,37 +36,31 @@ const trafficViewerApi = {...api}
   }, 500);
 
   const actionButtons = (window["isOasEnabled"] || window["isServiceMapEnabled"]) &&
-                          <div style={{ display: 'flex', height: "100%" }}>
-                              {window["isOasEnabled"] && <Button
-                                startIcon={<img className="custom" src={services} alt="services"></img>}
-                                size="large"
-                                variant="contained"
-                                className={commonClasses.outlinedButton + " " + commonClasses.imagedButton}
-                                style={{ marginRight: 25, textTransform: 'unset' }}
-                                onClick={handleOpenOasModal}>
-                                OpenAPI Specs
-                              </Button>}
-                              {window["isServiceMapEnabled"] && <Button
-                                startIcon={<img src={serviceMap} className="custom" alt="service-map" style={{marginRight:"8%"}}></img>}
-                                size="large"
-                                variant="contained"
-                                className={commonClasses.outlinedButton + " " + commonClasses.imagedButton}
-                                onClick={openServiceMapModalDebounce}
-                                style={{textTransform: 'unset'}}>
-                                Service Map
-                              </Button>}
-                        </div>
-
-  useEffect(() => {
-    return () => {
-      //closeSocket()
-    }
-  },[])
+    <div style={{ display: 'flex', height: "100%" }}>
+      {window["isOasEnabled"] && <Button
+        startIcon={<img className="custom" src={services} alt="services"></img>}
+        size="large"
+        variant="contained"
+        className={commonClasses.outlinedButton + " " + commonClasses.imagedButton}
+        style={{ marginRight: 25, textTransform: 'unset' }}
+        onClick={handleOpenOasModal}>
+        Service Catalog
+      </Button>}
+      {window["isServiceMapEnabled"] && <Button
+        startIcon={<img src={serviceMap} className="custom" alt="service-map" style={{ marginRight: "8%" }}></img>}
+        size="large"
+        variant="contained"
+        className={commonClasses.outlinedButton + " " + commonClasses.imagedButton}
+        onClick={openServiceMapModalDebounce}
+        style={{ textTransform: 'unset' }}>
+        Service Map
+      </Button>}
+    </div>
 
   return (
-  <>
+    <>
       <TrafficViewer setAnalyzeStatus={setAnalyzeStatus} webSocketUrl={MizuWebsocketURL} isCloseWebSocket={!openWebSocket}
-                     trafficViewerApiProp={trafficViewerApi} actionButtons={actionButtons} isShowStatusBar={!openOasModal} isDemoBannerView={false}/>
-  </>
+        trafficViewerApiProp={trafficViewerApi} actionButtons={actionButtons} isShowStatusBar={!(openOasModal || serviceMapModalOpen)} isDemoBannerView={false} />
+    </>
   );
 };
