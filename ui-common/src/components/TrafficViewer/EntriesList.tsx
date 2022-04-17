45df598bc9b4e8a1c31daa286db1c34567f47d0c
Leon <>
2022-04-17 17:10:13 +0300

    use const instead

--- ui-common/src/components/TrafficViewer/EntriesList.tsx
+++ ui-common/src/components/TrafficViewer/EntriesList.tsx
@@ -60,7 +60,7 @@ export const EntriesList: React.FC<EntriesListProps> = ({
   const [startTime, setStartTime] = useState(0);
   const [truncatedTimestamp, setTruncatedTimestamp] = useState(0);
 
-  const leftOffBottom = entries.length > 0 ? entries[entries.length - 1].id : "latest";
+  const leftOffBottom = entries.length > 0 ? entries[entries.length - 1].id : DEFAULT_LEFTOFF;
   const scrollbarVisible = scrollableRef.current?.childWrapperRef.current.clientHeight > scrollableRef.current?.wrapperRef.current.clientHeight;
 
   useEffect(() => {
