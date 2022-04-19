30986c3b221e3e7cd3ec89f14c99253aa1bd6595
leon-up9 <97597983+leon-up9@users.noreply.github.com>
2022-04-19 13:50:34 +0300

    Link component (#1005)
    * Link component
    
    * change defualt use
    
    * PR comments
    
    Co-authored-by: Leon <>

--- ui-common/src/components/UI/index.ts
+++ ui-common/src/components/UI/index.ts
@@ -5,10 +5,10 @@ import Tooltip from "./Tooltip";
 import Checkbox from "./Checkbox"
 import { StatusBar } from "./StatusBar";
 import CustomModal from "./CustomModal";
-import { InformationIcon } from "./InformationIcon";
+import { InformationIcon, Link } from "./InformationIcon";
 import SelectList from "./SelectList";
 import NoDataMessage from "./NoDataMessage";
 
 
-export { LoadingOverlay, Select, Tabs, Tooltip, Checkbox, CustomModal, InformationIcon, SelectList, NoDataMessage }
+export { LoadingOverlay, Select, Tabs, Tooltip, Checkbox, CustomModal, InformationIcon, SelectList, NoDataMessage, Link }
 export { StatusBar }
