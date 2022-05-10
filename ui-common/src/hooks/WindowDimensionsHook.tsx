9b4ced33b78c2d111b2740d69688d2fb6ff70a96
Amit Fainholts <amit@up9.com>
2022-05-10 16:16:15 +0300

    fix window width to not overlap between headlines

--- ui-common/src/hooks/WindowDimensionsHook.tsx
+++ ui-common/src/hooks/WindowDimensionsHook.tsx
@@ -14,7 +14,7 @@ export function useRequestTextByWidth(windowWidth){
     let responseText = "Response: "
     let elapsedTimeText = "Elapsed Time: "
 
-    if (windowWidth < 1433) {
+    if (windowWidth < 1436) {
         requestText = ""
         responseText = ""
         elapsedTimeText = ""
@@ -38,6 +38,6 @@ export default function useWindowDimensions() {
         window.addEventListener('resize', handleResize);
         return () => window.removeEventListener('resize', handleResize);
     }, []);
-
+    
     return windowDimensions;
 }
