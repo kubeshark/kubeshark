7086a47858a6cde400ef3b5070767ca9b6396f87
Nimrod Gilboa Markevich <nimrod@up9.com>
2022-05-14 22:30:46 +0300

    Plot packetsCount in analysis

--- performance_analysis/plot_from_tapper_logs.py
+++ performance_analysis/plot_from_tapper_logs.py
@@ -10,26 +10,29 @@ import typing
 # Input: List of log files
 
 def append_sample(name: str, line: str, samples: typing.List[float]):
-    pattern = name + r': (\d+\.\d+)'
+    pattern = name + r': ?(\d+(\.\d+)?)'
     maybe_sample = re.findall(pattern, line)
     if len(maybe_sample) == 0:
         return
 
-    sample = float(maybe_sample[0])
+    sample = float(maybe_sample[0][0])
     samples.append(sample)
 
 
-def extract_samples(f: typing.IO) -> typing.Tuple[pd.Series, pd.Series]:
+def extract_samples(f: typing.IO) -> typing.Tuple[pd.Series, pd.Series, pd.Series]:
     cpu_samples = []
     rss_samples = []
+    count_samples = []
     for line in f:
         append_sample('cpu', line, cpu_samples)
         append_sample('rss', line, rss_samples)
+        append_sample('"packetsCount"', line, count_samples)
 
     cpu_samples = pd.Series(cpu_samples)
     rss_samples = pd.Series(rss_samples)
+    count_samples = pd.Series(count_samples)
 
-    return cpu_samples, rss_samples
+    return cpu_samples, rss_samples, count_samples
 
 if __name__ == '__main__':
     filenames = sys.argv[1:]
@@ -38,28 +41,37 @@ if __name__ == '__main__':
 
     for ii, filename in enumerate(filenames):
         with open(filename, 'r') as f:
-            cpu_samples, rss_samples = extract_samples(f)
+            cpu_samples, rss_samples, count_samples = extract_samples(f)
 
         cpu_samples.name = pathlib.Path(filename).name
         rss_samples.name = pathlib.Path(filename).name
+        count_samples.name = pathlib.Path(filename).name
 
-        plt.subplot(2, 1, 1)
+        plt.subplot(3, 1, 1)
         cpu_samples.plot()
 
-        plt.subplot(2, 1, 2)
+        plt.subplot(3, 1, 2)
         (rss_samples / 1024 / 1024).plot()
-        plt.title('rss')
 
-    plt.subplot(2, 1, 1)
+        plt.subplot(3, 1, 3)
+        count_samples.plot()
+
+    plt.subplot(3, 1, 1)
     plt.legend()
     plt.title('cpu')
     plt.xlabel('# sample')
     plt.ylabel('cpu (%)')
 
-    plt.subplot(2, 1, 2)
+    plt.subplot(3, 1, 2)
     plt.title('rss')
     plt.legend()
     plt.xlabel('# sample')
     plt.ylabel('mem (MB)')
+
+    plt.subplot(3, 1, 3)
+    plt.title('packetsCount')
+    plt.legend()
+    plt.xlabel('# sample')
+    plt.ylabel('packetsCount')
 
     plt.show()
