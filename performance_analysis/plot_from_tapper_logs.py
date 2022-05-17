994ba4f192470c31ea6458ad07060f146ee5cac6
David Levanon <dvdlevanon@gmail.com>
2022-05-17 23:34:46 +0300

    add readme + run_tapper_benchmark.sh

--- performance_analysis/plot_from_tapper_logs.py
+++ performance_analysis/plot_from_tapper_logs.py
@@ -19,39 +19,67 @@ COLORMAP = plt.get_cmap('turbo')
 # Change group_pattern to an empty string to disable this, or change to a regex of your liking.
 
 
-def append_sample(name: str, line: str, samples: typing.List[float]):
+def get_sample(name: str, line: str, default_value: float):
     pattern = name + r': ?(\d+(\.\d+)?)'
     maybe_sample = re.findall(pattern, line)
     if len(maybe_sample) == 0:
-        return
+        return default_value
 
     sample = float(maybe_sample[0][0])
+    return sample
+
+
+def append_sample(name: str, line: str, samples: typing.List[float]):
+    sample = get_sample(name, line, -1)
+
+    if sample == -1:
+        return
+
     samples.append(sample)
 
 
-def extract_samples(f: typing.IO) -> typing.Tuple[pd.Series, pd.Series, pd.Series]:
+def extract_samples(f: typing.IO) -> typing.Tuple[pd.Series, pd.Series, pd.Series, pd.Series, pd.Series, pd.Series, pd.Series, pd.Series]:
     cpu_samples = []
     rss_samples = []
     count_samples = []
+    matched_samples = []
+    dropped_samples = []
+    processed_samples = []
+    heap_samples = []
+    goroutines_samples = []
     for line in f:
         append_sample('cpu', line, cpu_samples)
         append_sample('rss', line, rss_samples)
-        append_sample('"packetsCount"', line, count_samples)
+        tapper_packets_count = get_sample('"tapperPacketsCount"', line, -1)
+        packets_count = get_sample('"packetsCount"', line, -1)
+        if tapper_packets_count != -1 and packets_count != -1:
+            count_samples.append(packets_count - tapper_packets_count)
+        append_sample('"matchedPairs"', line, matched_samples)
+        append_sample('"droppedTcpStreams"', line, dropped_samples)
+        append_sample('"processedBytes"', line, processed_samples)
+        append_sample('mem', line, heap_samples)
+        append_sample('goroutines', line, goroutines_samples)
 
     cpu_samples = pd.Series(cpu_samples)
     rss_samples = pd.Series(rss_samples)
     count_samples = pd.Series(count_samples)
+    matched_samples = pd.Series(matched_samples)
+    dropped_samples = pd.Series(dropped_samples)
+    processed_samples = pd.Series(processed_samples)
+    heap_samples = pd.Series(heap_samples)
+    goroutines_samples = pd.Series(goroutines_samples)
 
-    return cpu_samples, rss_samples, count_samples
+    return cpu_samples, rss_samples, count_samples, matched_samples, dropped_samples, processed_samples, heap_samples, goroutines_samples
 
 
-def plot(df: pd.DataFrame, title: str, xlabel: str, ylabel: str, group_pattern: typing.Optional[str]):
+def plot(ax, df: pd.DataFrame, title: str, xlabel: str, ylabel: str, group_pattern: typing.Optional[str]):
     if group_pattern:
         color = get_group_color(df.columns, group_pattern)
         df.plot(color=color, ax=ax)
     else:
         df.plot(cmap=COLORMAP, ax=ax)
 
+    ax.ticklabel_format(style='plain')
     plt.title(title)
     plt.legend()
     plt.xlabel(xlabel)
@@ -74,32 +102,85 @@ if __name__ == '__main__':
     cpu_samples_all_files = []
     rss_samples_all_files = []
     count_samples_all_files = []
+    matched_samples_all_files = []
+    dropped_samples_all_files = []
+    processed_samples_all_files = []
+    heap_samples_all_files = []
+    goroutines_samples_all_files = []
 
     for ii, filename in enumerate(filenames):
+        if not filename.endswith(".log"):
+            continue
+
+        print("Analyzing {}".format(filename))
         with open(filename, 'r') as f:
-            cpu_samples, rss_samples, count_samples = extract_samples(f)
+            cpu_samples, rss_samples, count_samples, matched_samples, dropped_samples, processed_samples, heap_samples, goroutines_samples = extract_samples(f)
 
         cpu_samples.name = pathlib.Path(filename).name
         rss_samples.name = pathlib.Path(filename).name
         count_samples.name = pathlib.Path(filename).name
+        matched_samples.name = pathlib.Path(filename).name
+        dropped_samples.name = pathlib.Path(filename).name
+        processed_samples.name = pathlib.Path(filename).name
+        heap_samples.name = pathlib.Path(filename).name
+        goroutines_samples.name = pathlib.Path(filename).name
 
         cpu_samples_all_files.append(cpu_samples)
         rss_samples_all_files.append(rss_samples)
         count_samples_all_files.append(count_samples)
+        matched_samples_all_files.append(matched_samples)
+        dropped_samples_all_files.append(dropped_samples)
+        processed_samples_all_files.append(processed_samples)
+        heap_samples_all_files.append(processed_samples)
+        goroutines_samples_all_files.append(processed_samples)
 
     cpu_samples_df = pd.concat(cpu_samples_all_files, axis=1)
     rss_samples_df = pd.concat(rss_samples_all_files, axis=1)
     count_samples_df = pd.concat(count_samples_all_files, axis=1)
+    matched_samples_df = pd.concat(matched_samples_all_files, axis=1)
+    dropped_samples_df = pd.concat(dropped_samples_all_files, axis=1)
+    processed_samples_df = pd.concat(processed_samples_all_files, axis=1)
+
+    heap_samples_df = pd.concat(heap_samples_all_files, axis=1)
+    goroutines_samples_df = pd.concat(goroutines_samples_all_files, axis=1)
 
     group_pattern = r'^\d+'
 
-    ax = plt.subplot(3, 1, 1)
-    plot(cpu_samples_df, 'cpu', '# sample', 'cpu (%)', group_pattern)
+    cpu_plot = plt.subplot(8, 2, 1)
+    plot(cpu_plot, cpu_samples_df, 'cpu', '', 'cpu (%)', group_pattern)
+    cpu_plot.legend().remove()
 
-    ax = plt.subplot(3, 1, 2)
-    plot((rss_samples_df / 1024 / 1024), 'rss', '# sample', 'mem (MB)', group_pattern)
+    mem_plot = plt.subplot(8, 2, 2)
+    plot(mem_plot, (rss_samples_df / 1024 / 1024), 'rss', '', 'mem (mega)', group_pattern)
+    mem_plot.legend(loc='center left', bbox_to_anchor=(1, 0.5))
 
-    ax = plt.subplot(3, 1, 3)
-    plot(count_samples_df, 'packetsCount', '# sample', 'packetsCount', group_pattern)
+    packets_plot = plt.subplot(8, 2, 3)
+    plot(packets_plot, count_samples_df, 'packetsCount', '', 'packetsCount', group_pattern)
+    packets_plot.legend().remove()
 
-    plt.show()
+    matched_plot = plt.subplot(8, 2, 4)
+    plot(matched_plot, matched_samples_df, 'matchedCount', '', 'matchedCount', group_pattern)
+    matched_plot.legend().remove()
+
+    dropped_plot = plt.subplot(8, 2, 5)
+    plot(dropped_plot, dropped_samples_df, 'droppedCount', '', 'droppedCount', group_pattern)
+    dropped_plot.legend().remove()
+
+    processed_plot = plt.subplot(8, 2, 6)
+    plot(processed_plot, (processed_samples_df / 1024 / 1024), 'processedBytes', '', 'bytes (mega)', group_pattern)
+    processed_plot.legend().remove()
+
+    heap_plot = plt.subplot(8, 2, 7)
+    plot(heap_plot, (heap_samples_df / 1024 / 1024), 'heap', '', 'heap (mega)', group_pattern)
+    heap_plot.legend().remove()
+
+    goroutines_plot = plt.subplot(8, 2, 8)
+    plot(goroutines_plot, (goroutines_samples_df / 1024 / 1024), 'goroutines', '', 'goroutines', group_pattern)
+    goroutines_plot.legend().remove()
+
+    fig = plt.gcf()
+    fig.set_size_inches(20, 18)
+
+    print('Saving graph to graph.png')
+    plt.savefig('graph.png', bbox_inches='tight')
+    
