1360a80533b5a0e4ea3eae9400d76b93681f56ec
David Levanon <dvdlevanon@gmail.com>
2022-05-18 15:17:31 +0300

    Debug/profile tapper benchmark (#1093)
    * add mizu debug env to avoid all extensions
    
    * add readme + run_tapper_benchmark.sh
    
    * temporary change branch name
    
    * fix readme
    
    * fix MIZU_BENCHMARK_CLIENTS_COUNT env
    
    * change tap target to tcp stream
    
    * track live tcp streams
    
    * pr fixes
    
    * rename tapperPacketsCount to ignored_packets_count

--- tap/dbgctl/debug_control.go
+++ tap/dbgctl/debug_control.go
@@ -5,10 +5,11 @@ import (
 )
 
 var (
-	MizuTapperDisablePcap          bool = os.Getenv("MIZU_TAPPER_DISABLE_PCAP") == "true"
-	MizuTapperDisableTcpReassembly bool = os.Getenv("MIZU_TAPPER_DISABLE_TCP_REASSEMBLY") == "true"
-	MizuTapperDisableTapTarget     bool = os.Getenv("MIZU_TAPPER_DISABLE_TAP_TARGET") == "true"
-	MizuTapperDisableDissectors    bool = os.Getenv("MIZU_TAPPER_DISABLE_DISSECTORS") == "true"
-	MizuTapperDisableEmitting      bool = os.Getenv("MIZU_TAPPER_DISABLE_EMITTING") == "true"
-	MizuTapperDisableSending       bool = os.Getenv("MIZU_TAPPER_DISABLE_SENDING") == "true"
+	MizuTapperDisablePcap              bool = os.Getenv("MIZU_TAPPER_DISABLE_PCAP") == "true"
+	MizuTapperDisableTcpReassembly     bool = os.Getenv("MIZU_TAPPER_DISABLE_TCP_REASSEMBLY") == "true"
+	MizuTapperDisableTcpStream         bool = os.Getenv("MIZU_TAPPER_DISABLE_TCP_STREAM") == "true"
+	MizuTapperDisableDissectors        bool = os.Getenv("MIZU_TAPPER_DISABLE_DISSECTORS") == "true"
+	MizuTapperDisableEmitting          bool = os.Getenv("MIZU_TAPPER_DISABLE_EMITTING") == "true"
+	MizuTapperDisableSending           bool = os.Getenv("MIZU_TAPPER_DISABLE_SENDING") == "true"
+	MizuTapperDisableNonHttpExtensions bool = os.Getenv("MIZU_TAPPER_DISABLE_NON_HTTP_EXTENSSION") == "true"
 )
