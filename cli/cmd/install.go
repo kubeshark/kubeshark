45b368b33e9fe056d81a997477ce221720d6a7fb
RoyUP9 <87927115+RoyUP9@users.noreply.github.com>
2022-05-30 11:18:46 +0300

    Updated the install command UX (#1118)

--- cli/cmd/install.go
+++ cli/cmd/install.go
@@ -1,8 +1,11 @@
 package cmd
 
 import (
+	"github.com/creasty/defaults"
 	"github.com/spf13/cobra"
+	"github.com/up9inc/mizu/cli/config/configStructs"
 	"github.com/up9inc/mizu/cli/telemetry"
+	"github.com/up9inc/mizu/logger"
 )
 
 var installCmd = &cobra.Command{
@@ -17,4 +20,11 @@ var installCmd = &cobra.Command{
 
 func init() {
 	rootCmd.AddCommand(installCmd)
+
+	defaultInstallConfig := configStructs.InstallConfig{}
+	if err := defaults.Set(&defaultInstallConfig); err != nil {
+		logger.Log.Debug(err)
+	}
+
+	installCmd.Flags().BoolP(configStructs.OutInstallName, "o", defaultInstallConfig.Out, "print (to stdout) Kubernetes manifest used to install Mizu Pro edition")
 }
