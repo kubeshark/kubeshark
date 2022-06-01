5256902e9dca23415dd39f38b073ac4a76ba980d
M. Mert Yildiran <mehmet@up9.com>
2022-06-01 17:40:21 +0300

    Add malformed TODOs

--- tap/extensions/http/main.go
+++ tap/extensions/http/main.go
@@ -154,6 +154,8 @@ func (d dissecting) Dissect(b *bufio.Reader, reader api.TcpReader, options *api.
 		} else if reader.GetIsClient() {
 			var req *http.Request
 			switchingProtocolsHTTP2, req, err = handleHTTP1ClientStream(b, reader.GetReadProgress(), reader.GetParent().GetOrigin(), reader.GetTcpID(), reader.GetCounterPair(), reader.GetCaptureTime(), reader.GetEmitter(), options, reqResMatcher)
+			// TODO: Golang TLS malformed HTTP request
+			fmt.Printf("HTTP req err: %v\n", err)
 			if err == io.EOF || err == io.ErrUnexpectedEOF {
 				break
 			} else if err != nil {
@@ -187,7 +189,7 @@ func (d dissecting) Dissect(b *bufio.Reader, reader api.TcpReader, options *api.
 		} else {
 			switchingProtocolsHTTP2, err = handleHTTP1ServerStream(b, reader.GetReadProgress(), reader.GetParent().GetOrigin(), reader.GetTcpID(), reader.GetCounterPair(), reader.GetCaptureTime(), reader.GetEmitter(), options, reqResMatcher)
 			// TODO: Golang TLS malformed HTTP response
-			fmt.Printf("err: %v\n", err)
+			fmt.Printf("HTTP res err: %v\n", err)
 			if err == io.EOF || err == io.ErrUnexpectedEOF {
 				break
 			} else if err != nil {
