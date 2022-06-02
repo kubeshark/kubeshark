a8991a9880f7052787c9686234f8db940b39f001
Roee Gadot <roee.gadot@up9.com>
2022-06-02 14:33:12 +0300

    #run_acceptance_tests

--- acceptanceTests/cypress.config.js
+++ acceptanceTests/cypress.config.js
@@ -22,9 +22,9 @@ module.exports = defineConfig({
   e2e: {
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
-    setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.js')(on, config)
-    },
+    // setupNodeEvents(on, config) {
+    //   return require('./cypress/plugins/index.js')(on, config)
+    // },
     specPattern: 'cypress/e2e/tests/*.js',
     supportFile: false
   },
