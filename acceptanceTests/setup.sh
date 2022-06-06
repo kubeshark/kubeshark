873e0cebd9e401e39e8c39d952a4e1551b386ada
Roy Island <roy@up9.com>
2022-06-06 17:00:23 +0300

    change memory to 6946 #run_acceptance_tests

--- acceptanceTests/setup.sh
+++ acceptanceTests/setup.sh
@@ -27,7 +27,7 @@ else
 fi
 
 echo "Starting minikube..."
-minikube start --cpus 2 --memory 8192
+minikube start --cpus 2 --memory 6946
 
 echo "Creating mizu tests namespaces"
 kubectl create namespace mizu-tests --dry-run=client -o yaml | kubectl apply -f -
