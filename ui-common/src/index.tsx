e41488ef3eb2a473e4421615ab1a752d25408f9c
lirazyehezkel <61656597+lirazyehezkel@users.noreply.github.com>
2022-06-12 11:20:47 +0300

    TRA-4276 Folder structure refactor (#1131)
    * mizu-common folder structure refactor
    
    * no message
    
    * change images import from absolute to relative
    
    * relative sass imports
    
    * no message
    
    * remove tgz
    
    * spinner style

--- ui-common/src/index.tsx
+++ ui-common/src/index.tsx
@@ -2,10 +2,10 @@ import TrafficViewer from './components/TrafficViewer/TrafficViewer';
 import * as UI from "./components/UI"
 import { StatusBar } from './components/UI';
 import useWS, { DEFAULT_LEFTOFF } from './hooks/useWS';
-import OasModal from './components/OasModal/OasModal';
-import { ServiceMapModal } from './components/ServiceMapModal/ServiceMapModal';
+import OasModal from './components/modals/OasModal/OasModal';
+import { ServiceMapModal } from './components/modals/ServiceMapModal/ServiceMapModal';
 
-export { CodeEditorWrap as QueryForm } from './components/TrafficViewer/Filters';
+export { CodeEditorWrap as QueryForm } from './components/Filters/Filters';
 export { UI, StatusBar, OasModal, ServiceMapModal }
 export { useWS, DEFAULT_LEFTOFF }
 export default TrafficViewer;
