9f8e9cc91f083b9dee5b8cc8250df884619f1c1d
M. Mert Yildiran <mehmet@up9.com>
2022-06-12 18:29:25 +0300

    Define and use `BPF_CFLAGS` environment variable

--- tap/tlstapper/tls_tapper.go
+++ tap/tlstapper/tls_tapper.go
@@ -12,7 +12,7 @@ import (
 
 const GLOABL_TAP_PID = 0
 
-//go:generate go run github.com/cilium/ebpf/cmd/bpf2go@0d0727ef53e2f53b1731c73f4c61e0f58693083a -type tls_chunk tlsTapper bpf/tls_tapper.c -- -O2 -g -D__TARGET_ARCH_x86
+//go:generate go run github.com/cilium/ebpf/cmd/bpf2go@0d0727ef53e2f53b1731c73f4c61e0f58693083a -cflags $BPF_CFLAGS -type tls_chunk tlsTapper bpf/tls_tapper.c
 
 type TlsTapper struct {
 	bpfObjects      tlsTapperObjects
