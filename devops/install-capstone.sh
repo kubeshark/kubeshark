1afdc0307d16f00fe9b771630ba2b45c7c2949d5
M. Mert Yildiran <mehmet@up9.com>
2022-06-12 11:52:07 +0300

    Rename `aquynh` to `capstone-engine`

--- devops/install-capstone.sh
+++ devops/install-capstone.sh
@@ -5,7 +5,7 @@ if (( $EUID != 0 )); then
     SUDO='sudo'
 fi
 
-curl https://github.com/aquynh/capstone/archive/4.0.2.tar.gz -Lo ./capstone.tar.gz \
+curl https://github.com/capstone-engine/capstone/archive/4.0.2.tar.gz -Lo ./capstone.tar.gz \
  && tar -xzf capstone.tar.gz && mv ./capstone-* ./capstone \
  && cd capstone \
  && CAPSTONE_ARCHS="aarch64 x86" ./make.sh \
