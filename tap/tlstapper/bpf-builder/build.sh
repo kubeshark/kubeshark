541fff6a88f875d1074f6a492ba547153d9583b5
M. Mert Yildiran <mehmet@up9.com>
2022-06-20 15:47:20 +0300

    Remove big-endian files #run_acceptance_tests

--- tap/tlstapper/bpf-builder/build.sh
+++ tap/tlstapper/bpf-builder/build.sh
@@ -6,13 +6,11 @@ MIZU_HOME=$(realpath ../../../)
 
 docker build -t mizu-ebpf-builder . || exit 1
 
-BPF_TARGET_EL=amd64
-BPF_TARGET_EB=amd64
+BPF_TARGET=amd64
 BPF_CFLAGS="-O2 -g -D__TARGET_ARCH_x86"
 ARCH=$(uname -m)
 if [[ $ARCH == "aarch64" ]]; then
-    BPF_TARGET_EL=arm64
-    BPF_TARGET_EB=arm64be
+    BPF_TARGET=arm64
     BPF_CFLAGS="-O2 -g -D__TARGET_ARCH_arm64"
 fi
 
@@ -22,8 +20,7 @@ docker run --rm \
 	-v $(go env GOPATH):/root/go \
 	-it mizu-ebpf-builder \
 	sh -c "
-		BPF_TARGET=\"$BPF_TARGET_EL\" BPF_CFLAGS=\"$BPF_CFLAGS\" go generate tap/tlstapper/tls_tapper.go
-		BPF_TARGET=\"$BPF_TARGET_EB\" BPF_CFLAGS=\"$BPF_CFLAGS\" go generate tap/tlstapper/tls_tapper.go
+		BPF_TARGET=\"$BPF_TARGET\" BPF_CFLAGS=\"$BPF_CFLAGS\" go generate tap/tlstapper/tls_tapper.go
 		chown $(id -u):$(id -g) tap/tlstapper/tlstapper_bpf*
 	" || exit 1
 
