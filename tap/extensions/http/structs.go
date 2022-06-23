e5215a0c034b4a8bd045a1bdcd1fb490e06a14be
M. Mert Yildiran <mehmet@up9.com>
2022-06-23 19:13:39 +0300

    Update the HTTP dataset and bring back the sort #run_acceptance_tests

--- tap/extensions/http/structs.go
+++ tap/extensions/http/structs.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"fmt"
 	"net/http"
+	"sort"
 
 	"github.com/google/martian/har"
 )
@@ -31,6 +32,35 @@ func (h HTTPPayload) MarshalJSON() ([]byte, error) {
 		if err != nil {
 			return nil, errors.New("Failed converting request to HAR")
 		}
+		sort.Slice(harRequest.Headers, func(i, j int) bool {
+			if harRequest.Headers[i].Name < harRequest.Headers[j].Name {
+				return true
+			}
+			if harRequest.Headers[i].Name > harRequest.Headers[j].Name {
+				return false
+			}
+			return harRequest.Headers[i].Value < harRequest.Headers[j].Value
+		})
+		sort.Slice(harRequest.QueryString, func(i, j int) bool {
+			if harRequest.QueryString[i].Name < harRequest.QueryString[j].Name {
+				return true
+			}
+			if harRequest.QueryString[i].Name > harRequest.QueryString[j].Name {
+				return false
+			}
+			return harRequest.QueryString[i].Value < harRequest.QueryString[j].Value
+		})
+		if harRequest.PostData != nil {
+			sort.Slice(harRequest.PostData.Params, func(i, j int) bool {
+				if harRequest.PostData.Params[i].Name < harRequest.PostData.Params[j].Name {
+					return true
+				}
+				if harRequest.PostData.Params[i].Name > harRequest.PostData.Params[j].Name {
+					return false
+				}
+				return harRequest.PostData.Params[i].Value < harRequest.PostData.Params[j].Value
+			})
+		}
 		return json.Marshal(&HTTPWrapper{
 			Method:  harRequest.Method,
 			Url:     "",
@@ -41,6 +71,24 @@ func (h HTTPPayload) MarshalJSON() ([]byte, error) {
 		if err != nil {
 			return nil, errors.New("Failed converting response to HAR")
 		}
+		sort.Slice(harResponse.Headers, func(i, j int) bool {
+			if harResponse.Headers[i].Name < harResponse.Headers[j].Name {
+				return true
+			}
+			if harResponse.Headers[i].Name > harResponse.Headers[j].Name {
+				return false
+			}
+			return harResponse.Headers[i].Value < harResponse.Headers[j].Value
+		})
+		sort.Slice(harResponse.Cookies, func(i, j int) bool {
+			if harResponse.Cookies[i].Name < harResponse.Cookies[j].Name {
+				return true
+			}
+			if harResponse.Cookies[i].Name > harResponse.Cookies[j].Name {
+				return false
+			}
+			return harResponse.Cookies[i].Value < harResponse.Cookies[j].Value
+		})
 		return json.Marshal(&HTTPWrapper{
 			Method:  "",
 			Url:     "",
