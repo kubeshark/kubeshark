5fbd2bbe062380e0c19c9e23658f761ab51d1cd9
M. Mert Yildiran <mehmet@up9.com>
2022-06-28 13:45:48 +0300

    Pass `goid` offset to an eBPF map to retrieve it in eBPF context

--- tap/tlstapper/tlstapper_bpfel_x86.go
+++ tap/tlstapper/tlstapper_bpfel_x86.go
@@ -100,6 +100,7 @@ type tlsTapperMapSpecs struct {
 	FileDescriptorToIpv4 *ebpf.MapSpec `ebpf:"file_descriptor_to_ipv4"`
 	GoReadContext        *ebpf.MapSpec `ebpf:"go_read_context"`
 	GoWriteContext       *ebpf.MapSpec `ebpf:"go_write_context"`
+	GoidOffset           *ebpf.MapSpec `ebpf:"goid_offset"`
 	Heap                 *ebpf.MapSpec `ebpf:"heap"`
 	LogBuffer            *ebpf.MapSpec `ebpf:"log_buffer"`
 	OpensslReadContext   *ebpf.MapSpec `ebpf:"openssl_read_context"`
@@ -132,6 +133,7 @@ type tlsTapperMaps struct {
 	FileDescriptorToIpv4 *ebpf.Map `ebpf:"file_descriptor_to_ipv4"`
 	GoReadContext        *ebpf.Map `ebpf:"go_read_context"`
 	GoWriteContext       *ebpf.Map `ebpf:"go_write_context"`
+	GoidOffset           *ebpf.Map `ebpf:"goid_offset"`
 	Heap                 *ebpf.Map `ebpf:"heap"`
 	LogBuffer            *ebpf.Map `ebpf:"log_buffer"`
 	OpensslReadContext   *ebpf.Map `ebpf:"openssl_read_context"`
@@ -147,6 +149,7 @@ func (m *tlsTapperMaps) Close() error {
 		m.FileDescriptorToIpv4,
 		m.GoReadContext,
 		m.GoWriteContext,
+		m.GoidOffset,
 		m.Heap,
 		m.LogBuffer,
 		m.OpensslReadContext,
