a4bda9c36f1982172503316d4f65f4d9687e46ac
Nimrod Gilboa Markevich <nimrod@up9.com>
2022-06-29 09:56:48 +0000

    Print pid, source and destination addresses

--- tap/tlstapper/bpf/openssl_uprobes.c
+++ tap/tlstapper/bpf/openssl_uprobes.c
@@ -150,6 +150,8 @@ void BPF_KPROBE(ssl_ret_read_ex) {
 // Move to a separate file?
 SEC("kprobe/tcp_sendmsg")
 void BPF_KPROBE(tcp_sendmsg) {
+	long err;
+
 	__u64 id = bpf_get_current_pid_tgid();
 	__u32 pid = id >> 32;
 
@@ -158,6 +160,41 @@ void BPF_KPROBE(tcp_sendmsg) {
 		return;
 	}
 
+	struct sock *sk = (struct sock *) PT_REGS_PARM1(ctx);
+
+	short unsigned int family;
+	err = bpf_probe_read(&family, sizeof(family), (void *)&sk->__sk_common.skc_family);
+	if (err != 0) {
+		// TODO: Raise error
+		log_info(ctx, LOG_INFO_DEBUG, -1, -1, -1);
+		return;
+	}
+	// Check return code
+	if (family != AF_INET) {
+		return;
+	}
+
+	__be32 saddr;
+	__be32 daddr;
+	__be16 dport;
+	err = bpf_probe_read(&saddr, sizeof(saddr), (void *)&sk->__sk_common.skc_rcv_saddr);
+	if (err != 0) {
+		// TODO: Raise error
+		log_info(ctx, LOG_INFO_DEBUG, -1, -1, -1);
+		return;
+	}
+	err = bpf_probe_read(&daddr, sizeof(daddr), (void *)&sk->__sk_common.skc_daddr);
+	if (err != 0) {
+		// TODO: Raise error
+		log_info(ctx, LOG_INFO_DEBUG, -1, -1, -1);
+		return;
+	}
+	err = bpf_probe_read(&dport, sizeof(dport), (void *)&sk->__sk_common.skc_dport);
+	if (err != 0) {
+		// TODO: Raise error
+		log_info(ctx, LOG_INFO_DEBUG, -1, -1, -1);
+		return;
+	}
 	// Debug
-	log_info(ctx, LOG_INFO_DEBUG, pid, 0, 0);
+	log_info(ctx, LOG_INFO_DEBUG, pid, saddr, daddr);
 }
