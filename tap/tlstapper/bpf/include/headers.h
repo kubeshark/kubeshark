637eb7638af50fe46565ec8e6b30c30a5c7f90dd
M. Mert Yildiran <mehmet@up9.com>
2022-06-30 22:39:33 +0300

    Revert "Fix the issues for arm64 build"
    This reverts commit 48b041b1b60526658a1fa8774e5b24708af5b8a3.

--- tap/tlstapper/bpf/include/headers.h
+++ tap/tlstapper/bpf/include/headers.h
@@ -7,7 +7,6 @@ Copyright (C) UP9 Inc.
 #ifndef __HEADERS__
 #define __HEADERS__
 
-#include <stddef.h>
 #include "vmlinux.h"
 #include <bpf/bpf_helpers.h>
 #include "bpf/bpf_tracing.h"
--- tap/tlstapper/tlstapper_bpfel_arm64.go
+++ tap/tlstapper/tlstapper_bpfel_arm64.go
@@ -13,11 +13,6 @@ import (
 	"github.com/cilium/ebpf"
 )
 
-type tlsTapperGoidOffsets struct {
-	G_addrOffset uint64
-	GoidOffset   uint64
-}
-
 type tlsTapperTlsChunk struct {
 	Pid      uint32
 	Tgid     uint32
@@ -105,7 +100,7 @@ type tlsTapperMapSpecs struct {
 	FileDescriptorToIpv4 *ebpf.MapSpec `ebpf:"file_descriptor_to_ipv4"`
 	GoReadContext        *ebpf.MapSpec `ebpf:"go_read_context"`
 	GoWriteContext       *ebpf.MapSpec `ebpf:"go_write_context"`
-	GoidOffsetsMap       *ebpf.MapSpec `ebpf:"goid_offsets_map"`
+	GoidOffsetMap        *ebpf.MapSpec `ebpf:"goid_offset_map"`
 	Heap                 *ebpf.MapSpec `ebpf:"heap"`
 	LogBuffer            *ebpf.MapSpec `ebpf:"log_buffer"`
 	OpensslReadContext   *ebpf.MapSpec `ebpf:"openssl_read_context"`
@@ -139,7 +134,7 @@ type tlsTapperMaps struct {
 	FileDescriptorToIpv4 *ebpf.Map `ebpf:"file_descriptor_to_ipv4"`
 	GoReadContext        *ebpf.Map `ebpf:"go_read_context"`
 	GoWriteContext       *ebpf.Map `ebpf:"go_write_context"`
-	GoidOffsetsMap       *ebpf.Map `ebpf:"goid_offsets_map"`
+	GoidOffsetMap        *ebpf.Map `ebpf:"goid_offset_map"`
 	Heap                 *ebpf.Map `ebpf:"heap"`
 	LogBuffer            *ebpf.Map `ebpf:"log_buffer"`
 	OpensslReadContext   *ebpf.Map `ebpf:"openssl_read_context"`
@@ -156,7 +151,7 @@ func (m *tlsTapperMaps) Close() error {
 		m.FileDescriptorToIpv4,
 		m.GoReadContext,
 		m.GoWriteContext,
-		m.GoidOffsetsMap,
+		m.GoidOffsetMap,
 		m.Heap,
 		m.LogBuffer,
 		m.OpensslReadContext,
