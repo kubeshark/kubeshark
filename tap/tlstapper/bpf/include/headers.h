7d309120aa8f225cb87d8876e68b292c394e75bb
M. Mert Yildiran <mehmet@up9.com>
2022-06-30 14:09:22 +0300

    Add `user_pt_regs`

--- tap/tlstapper/bpf/include/headers.h
+++ tap/tlstapper/bpf/include/headers.h
@@ -11,4 +11,11 @@ Copyright (C) UP9 Inc.
 #include <bpf/bpf_helpers.h>
 #include "bpf/bpf_tracing.h"
 
+struct user_pt_regs {
+	__u64		regs[31];
+	__u64		sp;
+	__u64		pc;
+	__u64		pstate;
+};
+
 #endif /* __HEADERS__ */
