b4bc09637c378a935ce3299383630257a34947df
AmitUp9 <96980485+AmitUp9@users.noreply.github.com>
2022-07-03 11:48:38 +0300

    fix select in traffic stats ui (#1179)

--- ui-common/src/components/modals/TrafficStatsModal/TrafficStatsModal.tsx
+++ ui-common/src/components/modals/TrafficStatsModal/TrafficStatsModal.tsx
@@ -34,14 +34,14 @@ interface TrafficStatsModalProps {
   getTimelineStatsDataApi: () => Promise<any>
 }
 
-export const PROTOCOLS = ["ALL PROTOCOLS","gRPC", "REDIS", "HTTP", "GQL", "AMQP", "KFAKA"];
+export const PROTOCOLS = ["ALL", "gRPC", "REDIS", "HTTP", "GQL", "AMQP", "KFAKA"];
 export const ALL_PROTOCOLS = PROTOCOLS[0];
 
 export const TrafficStatsModal: React.FC<TrafficStatsModalProps> = ({ isOpen, onClose, getPieStatsDataApi, getTimelineStatsDataApi }) => {
 
   const modes = Object.keys(StatsMode).filter(x => !(parseInt(x) >= 0));
   const [statsMode, setStatsMode] = useState(modes[0]);
-  const [selectedProtocol, setSelectedProtocol] = useState("ALL PROTOCOLS");
+  const [selectedProtocol, setSelectedProtocol] = useState(ALL_PROTOCOLS);
   const [pieStatsData, setPieStatsData] = useState(null);
   const [timelineStatsData, setTimelineStatsData] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
