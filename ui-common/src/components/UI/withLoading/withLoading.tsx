2ad396f51ea585f073d4c6d22e16426066260646
Leon <>
2022-07-03 20:42:05 +0300

    withLoading

---
+++ ui-common/src/components/UI/withLoading/withLoading.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import spinner from 'spinner.svg';
+
+export interface WithLoadingProps {
+    isLoading: boolean
+    loaderMargin: number,
+    loaderHeight: number
+}
+
+const withLoading = <P extends object>(
+    Component: React.ComponentType<P>
+): React.FC<P & WithLoadingProps> => ({
+    isLoading,
+    loaderMargin = 20,
+    loaderHeight = 35,
+    ...props
+}: WithLoadingProps) => isLoading ? <div style={{ textAlign: "center", margin: 20 }}>
+    <img alt="spinner" src={spinner} style={{ height: 35 }} />
+</div> : <Component {...props as P} />;
+
+
+export default withLoading
