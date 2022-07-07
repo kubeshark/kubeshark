a0c1ae603b8d986bfeb44b3a0ada7b775583a649
Nimrod Gilboa Markevich <nimrod@up9.com>
2022-07-07 22:39:16 +0300

    Support mac M1 in bpf-builder script

--- tap/tlstapper/bpf-builder/build.sh
+++ tap/tlstapper/bpf-builder/build.sh
@@ -9,7 +9,7 @@ docker build -t mizu-ebpf-builder . || exit 1
 BPF_TARGET=amd64
 BPF_CFLAGS="-O2 -g -D__TARGET_ARCH_x86"
 ARCH=$(uname -m)
-if [[ $ARCH == "aarch64" ]]; then
+if [[ $ARCH == "aarch64" || $ARCH == "arm64" ]]; then
     BPF_TARGET=arm64
     BPF_CFLAGS="-O2 -g -D__TARGET_ARCH_arm64"
 fi
