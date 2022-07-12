7c159fffc0a4f0d29cbb71f6e1772b517293c69c
RoyUP9 <87927115+RoyUP9@users.noreply.github.com>
2022-07-12 10:19:24 +0300

    Added redact using insertion filter (#1196)

--- tap/api/options.go
+++ tap/api/options.go
@@ -1,7 +1,5 @@
 package api
 
 type TrafficFilteringOptions struct {
-	IgnoredUserAgents       []string
-	PlainTextMaskingRegexes []*SerializableRegexp
-	EnableRedaction         bool
+	IgnoredUserAgents []string
 }
