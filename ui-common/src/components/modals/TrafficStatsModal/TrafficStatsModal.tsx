e9719cba3a6bcf259e01880d6d3b9dcf738f03ae
gadotroee <55343099+gadotroee@users.noreply.github.com>
2022-07-13 17:21:18 +0300

    Add time range to stats  (#1199)

--- ui-common/src/components/modals/TrafficStatsModal/TrafficStatsModal.tsx
+++ ui-common/src/components/modals/TrafficStatsModal/TrafficStatsModal.tsx
@@ -1,13 +1,14 @@
 import React, { useCallback, useEffect, useState } from "react";
-import { Backdrop, Box, Button, debounce, Fade, Modal } from "@mui/material";
+import { Backdrop, Box, debounce, Fade, Modal } from "@mui/material";
 import styles from "./TrafficStatsModal.module.sass";
 import closeIcon from "assets/close.svg";
 import { TrafficPieChart } from "./TrafficPieChart/TrafficPieChart";
 import { TimelineBarChart } from "./TimelineBarChart/TimelineBarChart";
-import refreshIcon from "assets/refresh.svg";
-import { useCommonStyles } from "../../../helpers/commonStyle";
 import { LoadingWrapper } from "../../UI/withLoading/withLoading";
 import { ALL_PROTOCOLS, StatsMode } from "./consts";
+import { TimeRangePicker } from "./TimelineBarChart/TimeRangePicker/TimeTangePicker";
+import { EuiProvider } from '@elastic/eui';
+import '@elastic/eui/dist/eui_theme_light.css';
 
 const modalStyle = {
   position: 'absolute',
@@ -15,7 +16,7 @@ const modalStyle = {
   left: '50%',
   transform: 'translate(-50%, 0%)',
   width: '60vw',
-  height: '82vh',
+  height: '90vh',
   bgcolor: 'background.paper',
   borderRadius: '5px',
   boxShadow: 24,
@@ -26,11 +27,10 @@ const modalStyle = {
 interface TrafficStatsModalProps {
   isOpen: boolean;
   onClose: () => void;
-  getTrafficStatsDataApi: () => Promise<any>
+  getTrafficStatsDataApi: (start?, end?) => Promise<any>
 }
 
 export const TrafficStatsModal: React.FC<TrafficStatsModalProps> = ({ isOpen, onClose, getTrafficStatsDataApi }) => {
-
   const modes = Object.keys(StatsMode).filter(x => !(parseInt(x) >= 0));
   const [statsMode, setStatsMode] = useState(modes[0]);
   const [selectedProtocol, setSelectedProtocol] = useState(ALL_PROTOCOLS);
@@ -38,14 +38,13 @@ export const TrafficStatsModal: React.FC<TrafficStatsModalProps> = ({ isOpen, on
   const [timelineStatsData, setTimelineStatsData] = useState(null);
   const [protocols, setProtocols] = useState([])
   const [isLoading, setIsLoading] = useState(false);
-  const commonClasses = useCommonStyles();
 
-  const getTrafficStats = useCallback(async () => {
+  const getTrafficStats = useCallback(async (startTime, endTime) => {
     if (isOpen && getTrafficStatsDataApi) {
       (async () => {
         try {
           setIsLoading(true);
-          const statsData = await getTrafficStatsDataApi();
+          const statsData = await getTrafficStatsDataApi(startTime, endTime);
           setPieStatsData(statsData.pie);
           setTimelineStatsData(statsData.timeline);
           setProtocols(statsData.protocols)
@@ -59,65 +58,63 @@ export const TrafficStatsModal: React.FC<TrafficStatsModalProps> = ({ isOpen, on
   }, [isOpen, getTrafficStatsDataApi, setPieStatsData, setTimelineStatsData])
 
   useEffect(() => {
-    getTrafficStats();
+    const now = new Date().getTime();
+    const halfAnHourAgo = now - (30 * 60 * 1000);
+    getTrafficStats(halfAnHourAgo, now);
   }, [getTrafficStats])
 
-  const refreshStats = debounce(() => {
-    getTrafficStats();
+  const refreshStats = debounce((newStartTime, newEndTime) => {
+    getTrafficStats(newStartTime, newEndTime);
   }, 500);
 
   return (
-    <Modal
-      aria-labelledby="transition-modal-title"
-      aria-describedby="transition-modal-description"
-      open={isOpen}
-      onClose={onClose}
-      closeAfterTransition
-      BackdropComponent={Backdrop}
-      BackdropProps={{ timeout: 500 }}>
-      <Fade in={isOpen}>
-        <Box sx={modalStyle}>
-          <div className={styles.closeIcon}>
-            <img src={closeIcon} alt="close" onClick={() => onClose()} style={{ cursor: "pointer", userSelect: "none" }} />
-          </div>
-          <div className={styles.headlineContainer}>
-            <div className={styles.title}>Traffic Statistics</div>
-            <Button style={{ marginLeft: "2%", textTransform: 'unset' }}
-              startIcon={<img src={refreshIcon} className="custom" alt="refresh"></img>}
-              size="medium"
-              variant="contained"
-              className={commonClasses.outlinedButton + " " + commonClasses.imagedButton}
-              onClick={refreshStats}
-            >
-              Refresh
-            </Button>
-          </div>
-          <div className={styles.mainContainer}>
-            <div className={styles.selectContainer}>
-              <div>
-                <span style={{ marginRight: 15 }}>Breakdown By</span>
-                <select className={styles.select} value={statsMode} onChange={(e) => setStatsMode(e.target.value)}>
-                  {modes.map(mode => <option key={mode} value={mode}>{mode}</option>)}
-                </select>
-              </div>
-              <div>
-                <span style={{ marginRight: 15 }}>Protocol</span>
-                <select className={styles.select} value={selectedProtocol} onChange={(e) => setSelectedProtocol(e.target.value)}>
-                  {protocols.map(protocol => <option key={protocol} value={protocol}>{protocol}</option>)}
-                </select>
-              </div>
+    <EuiProvider>
+      <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        open={isOpen}
+        onClose={onClose}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{ timeout: 500 }}>
+        <Fade in={isOpen}>
+          <Box sx={modalStyle}>
+            <div className={styles.closeIcon}>
+              <img src={closeIcon} alt="close" onClick={() => onClose()} style={{ cursor: "pointer", userSelect: "none" }} />
             </div>
-            <div>
-              <LoadingWrapper isLoading={isLoading} loaderMargin={20} loaderHeight={50}>
+            <div className={styles.headlineContainer}>
+              <div className={styles.title}>Traffic Statistics</div>
+            </div>
+            <div className={styles.mainContainer}>
+              <div className={styles.selectContainer}>
                 <div>
-                  <TrafficPieChart pieChartMode={statsMode} data={pieStatsData} selectedProtocol={selectedProtocol} />
-                  <TimelineBarChart timeLineBarChartMode={statsMode} data={timelineStatsData} selectedProtocol={selectedProtocol} />
+                  <TimeRangePicker refreshStats={refreshStats} />
                 </div>
-              </LoadingWrapper>
+                <div>
+                  <span style={{ marginRight: 15 }}>Breakdown By</span>
+                  <select className={styles.select} value={statsMode} onChange={(e) => setStatsMode(e.target.value)}>
+                    {modes.map(mode => <option key={mode} value={mode}>{mode}</option>)}
+                  </select>
+                </div>
+                <div>
+                  <span style={{ marginRight: 15 }}>Protocol</span>
+                  <select className={styles.select} value={selectedProtocol} onChange={(e) => setSelectedProtocol(e.target.value)}>
+                    {protocols.map(protocol => <option key={protocol} value={protocol}>{protocol}</option>)}
+                  </select>
+                </div>
+              </div>
+              <div>
+                <LoadingWrapper isLoading={isLoading} loaderMargin={20} loaderHeight={50}>
+                  <div>
+                    <TrafficPieChart pieChartMode={statsMode} data={pieStatsData} selectedProtocol={selectedProtocol} />
+                    <TimelineBarChart timeLineBarChartMode={statsMode} data={timelineStatsData} selectedProtocol={selectedProtocol} />
+                  </div>
+                </LoadingWrapper>
+              </div>
             </div>
-          </div>
-        </Box>
-      </Fade>
-    </Modal>
+          </Box>
+        </Fade>
+      </Modal>
+    </EuiProvider>
   );
 }
