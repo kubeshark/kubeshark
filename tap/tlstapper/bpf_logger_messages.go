d371a0984410464d2b241c7ba8a3e750d87f724b
Nimrod Gilboa Markevich <nimrod@up9.com>
2022-07-19 11:05:38 +0000

    Fix message numbering commnet in go

--- tap/tlstapper/bpf_logger_messages.go
+++ tap/tlstapper/bpf_logger_messages.go
@@ -4,7 +4,7 @@ package tlstapper
 //
 var bpfLogMessages = []string{
 	/*0000*/ "[%d] Unable to read bytes count from _ex methods [err: %d]",
-	/*0002*/ "[%d] Unable to read ssl buffer [err: %d] [origin: %d]",
+	/*0001*/ "[%d] Unable to read ssl buffer [err: %d] [origin: %d]",
 	/*0002*/ "[%d] Buffer is too big [size: %d]",
 	/*0003*/ "[%d] Unable to allocate chunk in bpf heap",
 	/*0004*/ "[%d] Unable to read ssl context [err: %d] [origin: %d]",
@@ -22,7 +22,7 @@ var bpfLogMessages = []string{
 	/*0016*/ "[%d] Unable to read socket family [err: %d]",
 	/*0017*/ "[%d] Unable to read socket daddr [err: %d]",
 	/*0018*/ "[%d] Unable to read socket saddr [err: %d]",
-	/*0018*/ "[%d] Unable to read socket dport [err: %d]",
+	/*0019*/ "[%d] Unable to read socket dport [err: %d]",
 	/*0020*/ "[%d] Unable to read socket sport [err: %d]",
 	/*0021*/ "[%d] Unable to put go user-kernel context [fd: %d] [err: %d]",
 	/*0022*/ "[%d] Unable to get go user-kernel context [fd: %d]]",
