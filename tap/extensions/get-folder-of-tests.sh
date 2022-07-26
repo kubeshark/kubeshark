710de80520b8e277578a162b1b222dfa35df351f
Roee Gadot <roee.gadot@up9.com>
2022-07-26 12:32:43 +0300

    cr fixes

--- tap/extensions/get-folder-of-tests.sh
+++ tap/extensions/get-folder-of-tests.sh
@@ -3,20 +3,21 @@ mizu_test_files_tmp_folder="mizu-test-files-tmp"
 
 requested_folder=$1
 destination_folder_name=$2
-echo "Going to download folder (${requested_folder}) from repo (${mizu_test_files_repo}) and save it to local fodler (${destination_folder_name}"
+echo "Going to download folder (${requested_folder}) from repo (${mizu_test_files_repo}) and save it to local folder (${destination_folder_name})"
 
+echo "Cloning repo to tmp folder (${mizu_test_files_tmp_folder})"
 git clone ${mizu_test_files_repo} ${mizu_test_files_tmp_folder} --no-checkout --depth 1 --filter=blob:none --sparse
-echo "Cloneing repo to tmp folder (${mizu_test_files_tmp_folder})"
 cd ${mizu_test_files_tmp_folder}
 
-git sparse-checkout add ${requested_folder}
 echo "Adding sparse checkout folder"
+git sparse-checkout add ${requested_folder}
 
-git checkout 
+
 echo "Checkout"
+git checkout 
 
-mv ${requested_folder} ../${destination_folder_name}
 echo "Moving folder to the destination location"
+mv ${requested_folder} ../${destination_folder_name}
 
 cd ..
 echo "Removing the tmp folder"
