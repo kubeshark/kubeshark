b0dae268b1ea4b862b5c5805e0a7a925c1a01be5
M. Mert Yildiran <me@mertyildiran.com>
2022-11-24 20:50:22 +0300

    Remove the `agent` directory

--- agent/pkg/version/consts.go
+++
@@ -1,8 +0,0 @@
-package version
-
-var (
-	Ver         = "0.0.1"
-	Branch         = "develop"
-	GitCommitHash  = "" // this var is overridden using ldflags in makefile when building
-	BuildTimestamp = "" // this var is overridden using ldflags in makefile when building
-)
