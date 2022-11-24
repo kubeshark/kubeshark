817caa79738bfaad9504f09de96892fa91e71232
M. Mert Yildiran <me@mertyildiran.com>
2022-11-24 20:04:10 +0300

    Remove `devops` directory

--- devops/linux-arm64-musl-go-libpcap-capstone-bpf/build-push.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e
-
-# Build it on x86_64
-docker build . -t kubeshark/linux-arm64-musl-go-libpcap-capstone-bpf:capstone-5.0-rc2 && docker push kubeshark/linux-arm64-musl-go-libpcap-capstone-bpf:capstone-5.0-rc2
--- devops/linux-x86_64-musl-go-libpcap-capstone-bpf/build-push.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e
-
-# Build it on arm64
-docker build . -t kubeshark/linux-x86_64-musl-go-libpcap-capstone-bpf:capstone-5.0-rc2 && docker push kubeshark/linux-x86_64-musl-go-libpcap-capstone-bpf:capstone-5.0-rc2
