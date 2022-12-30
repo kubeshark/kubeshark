7e7fa772e164d6eaa79e94926aa59737ac6c7731
M. Mert Yildiran <me@mertyildiran.com>
2022-12-30 08:30:48 +0300

    :bug: Have a short caller (file:line) log

--- kubeshark.go
+++ kubeshark.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"os"
+	"strconv"
 	"time"
 
 	"github.com/kubeshark/kubeshark/cmd"
@@ -11,6 +12,20 @@ import (
 
 func main() {
 	zerolog.SetGlobalLevel(zerolog.InfoLevel)
+
+	// Short caller (file:line)
+	zerolog.CallerMarshalFunc = func(pc uintptr, file string, line int) string {
+		short := file
+		for i := len(file) - 1; i > 0; i-- {
+			if file[i] == '/' {
+				short = file[i+1:]
+				break
+			}
+		}
+		file = short
+		return file + ":" + strconv.Itoa(line)
+	}
+
 	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr, TimeFormat: time.RFC3339}).With().Caller().Logger()
 	cmd.Execute()
 }
