39f8d40b76d5bc18592fd07fd4aad034b94e26a1
M. Mert Yildiran <me@mertyildiran.com>
2023-05-24 02:10:48 +0300

    Revert ":zap: Add `Refresh-Token` to the list of `Access-Control-Allow-Headers`"
    This reverts commit bf731073c87688e16d2e19f47ef73152c091c290.

--- cmd/pro.go
+++ cmd/pro.go
@@ -85,7 +85,7 @@ func runLicenseRecieverServer() {
 	ginApp.Use(func(c *gin.Context) {
 		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
-		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With, x-session-token, Refresh-Token")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With, x-session-token")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
 		c.Writer.Header().Set("Access-Control-Expose-Headers", "Content-Disposition")
 
--- kubernetes/proxy.go
+++ kubernetes/proxy.go
@@ -74,7 +74,7 @@ func getRerouteHttpHandlerSelfAPI(proxyHandler http.Handler, selfNamespace strin
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With, x-session-token, Refresh-Token")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With, x-session-token")
 		w.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
 
 		if r.Method == "OPTIONS" {
