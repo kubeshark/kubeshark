65ab0ca668ef81ab058cd6ccaa4584faa2bda0e5
M. Mert Yildiran <me@mertyildiran.com>
2023-09-21 21:52:43 +0300

    :bug: Don't use `encoding/json` in `config` command

--- utils/pretty.go
+++ utils/pretty.go
@@ -2,28 +2,15 @@ package utils
 
 import (
 	"bytes"
-	"encoding/json"
 
 	"github.com/goccy/go-yaml"
 )
 
 func PrettyYaml(data interface{}) (result string, err error) {
-	var marshalled []byte
-	marshalled, err = json.Marshal(data)
-	if err != nil {
-		return
-	}
-
-	var unmarshalled interface{}
-	err = json.Unmarshal(marshalled, &unmarshalled)
-	if err != nil {
-		return
-	}
-
 	buffer := new(bytes.Buffer)
 	encoder := yaml.NewEncoder(buffer, yaml.Indent(2))
 
-	err = encoder.Encode(unmarshalled)
+	err = encoder.Encode(data)
 	if err != nil {
 		return
 	}
