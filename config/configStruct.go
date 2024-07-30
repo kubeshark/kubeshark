9c9cefc406e312eddad728ed0b2fdd8d0f8bb819
Alon Girmonsky <1990761+alongir@users.noreply.github.com>
2024-07-29 17:16:21 -0700

    Change `supportChatEnabled` to be `true` by default.

--- config/configStruct.go
+++ config/configStruct.go
@@ -95,7 +95,7 @@ type ConfigStruct struct {
 	HeadlessMode        bool                          `yaml:"headless" json:"headless" default:"false"`
 	License             string                        `yaml:"license" json:"license" default:""`
 	CloudLicenseEnabled bool                          `yaml:"cloudLicenseEnabled" json:"cloudLicenseEnabled" default:"true"`
-	SupportChatEnabled  bool                          `yaml:"supportChatEnabled" json:"supportChatEnabled" default:"false"`
+	SupportChatEnabled  bool                          `yaml:"supportChatEnabled" json:"supportChatEnabled" default:"true"`
 	Scripting           configStructs.ScriptingConfig `yaml:"scripting" json:"scripting"`
 	Manifests           ManifestsConfig               `yaml:"manifests,omitempty" json:"manifests,omitempty"`
 	Timezone            string                        `yaml:"timezone" json:"timezone"`
