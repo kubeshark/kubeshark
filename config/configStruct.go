631e5f2d24f4c2116a3a2f78646abc37b1db5ee3
Serhii Ponomarenko <116438358+tiptophelmet@users.noreply.github.com>
2025-02-21 03:25:58 +0200

    🔨 Add `demoModeEnabled` helm value (#1714)
    * 🔨 Add `demoModeEnabled` helm value
    
    * :bug: Fix `demoModeEnabled` ternary expressions
    
    * :safety_vest: Check `demoModeEnabled` existence

--- config/configStruct.go
+++ config/configStruct.go
@@ -150,6 +150,7 @@ type ConfigStruct struct {
 	HeadlessMode         bool                          `yaml:"headless" json:"headless" default:"false"`
 	License              string                        `yaml:"license" json:"license" default:""`
 	CloudLicenseEnabled  bool                          `yaml:"cloudLicenseEnabled" json:"cloudLicenseEnabled" default:"true"`
+	DemoModeEnabled      bool                          `yaml:"demoModeEnabled" json:"demoModeEnabled" default:"false"`
 	SupportChatEnabled   bool                          `yaml:"supportChatEnabled" json:"supportChatEnabled" default:"true"`
 	InternetConnectivity bool                          `yaml:"internetConnectivity" json:"internetConnectivity" default:"true"`
 	Scripting            configStructs.ScriptingConfig `yaml:"scripting" json:"scripting"`
