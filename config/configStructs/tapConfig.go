1e88f9ca024394a9492641ac95839ef5998ec610
Volodymyr Stoiko <me@volodymyrstoiko.com>
2025-09-18 13:42:11 +0300

    Use badger max db size option

--- config/configStructs/tapConfig.go
+++ config/configStructs/tapConfig.go
@@ -301,9 +301,9 @@ type SeLinuxOptionsConfig struct {
 }
 
 type CaptureConfig struct {
-	Stopped         bool    `yaml:"stopped" json:"stopped" default:"false"`
-	StopAfter       string  `yaml:"stopAfter" json:"stopAfter" default:"5m"`
-	DbMaxSizeRatio float64 `yaml:"dbMaxSizeRatio" json:"dbMaxSizeRatio" default:"0.8"`
+	Stopped     bool   `yaml:"stopped" json:"stopped" default:"false"`
+	StopAfter   string `yaml:"stopAfter" json:"stopAfter" default:"5m"`
+	DbMaxSize   string `yaml:"dbMaxSize" json:"dbMaxSize" default:""`
 }
 
 type TapConfig struct {
